7c6e4ead57a83370f38fc17be8c5445645fff03f
Jonathan Percival <jonathan.i.percival@gmail.com>
2019-10-02 11:24:43 -0600

    Break out service / rest layerscd

--- rest/src/main/java/com/alphora/cql/rest/Main.java
+++ rest/src/main/java/com/alphora/cql/rest/Main.java
@@ -1,7 +1,13 @@
 package com.alphora.cql.rest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        Server.build().start();  
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
 }

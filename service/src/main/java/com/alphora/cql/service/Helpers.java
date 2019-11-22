5480112b4e7de2a96480cf1885f085838d4de619
Jonathan Percival <jonathan.i.percival@gmail.com>
2019-11-21 20:55:37 -0700

    Fix bug in detecting file paths

--- service/src/main/java/com/alphora/cql/service/Helpers.java
+++ service/src/main/java/com/alphora/cql/service/Helpers.java
@@ -2,6 +2,6 @@ package com.alphora.cql.service;
 
 public class Helpers {
     public static boolean isFileUri(String uri) {
-        return uri.startsWith("file") || !uri.matches("\\w+?://");
+        return uri.startsWith("file") || !uri.matches("\\w+?://.*");
     }
 }

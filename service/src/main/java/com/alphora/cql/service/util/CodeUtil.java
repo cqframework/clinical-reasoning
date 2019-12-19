0e6a5e8dfc584071ee4ee5ecb4a9e0cd3a33796c
jreyno77 <inujosha1@gmail.com>
2019-12-19 11:59:12 -0700

    now truly version independent needs clean up

--- service/src/main/java/com/alphora/cql/service/util/CodeUtil.java
+++ service/src/main/java/com/alphora/cql/service/util/CodeUtil.java
@@ -1,74 +1,151 @@
 package com.alphora.cql.service.util;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.hl7.fhir.instance.model.api.IBase;
+import org.hl7.fhir.instance.model.api.IPrimitiveType;
 import org.opencds.cqf.cql.runtime.Code;
 
+import ca.uhn.fhir.context.BaseRuntimeChildDefinition;
+import ca.uhn.fhir.context.BaseRuntimeElementDefinition;
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.RuntimeCompositeDatatypeDefinition;
+import ca.uhn.fhir.context.BaseRuntimeChildDefinition.IAccessor;
 
 public class CodeUtil {
 
-    public static ArrayList<Code> getCodesFromCodeableConceptsObject(Object codes, FhirContext fhirContext) {
-        switch (fhirContext.getVersion().getVersion()) {
-            case DSTU3:
-                return getDSTU3CodesFromCodeableConceptsObject(codes);
-            case R4:
-                return getR4CodesFromCodeableConceptsObject(codes);
-            default:
-                throw new IllegalArgumentException("Unknown fhir version: " + fhirContext.getVersion().getVersion().getFhirVersionString());
+    public static List<Code> getElmCodesFromObject(Object object, FhirContext fhirContext) {
+        List<Code> codesInIterable = getCodesInIterable(fhirContext, object);
+        List<Code> codesInConcept = getCodesInConcept(fhirContext, object);
+        List<Code> codesInCoding = getCodesInCoding(fhirContext, object);
+
+        if (codesInIterable != null && !codesInIterable.isEmpty()) {
+            return codesInIterable;
         }
+
+        if (codesInConcept != null && !codesInConcept.isEmpty()) {
+            return codesInConcept;
+        }
+
+        if (codesInCoding != null && !codesInCoding.isEmpty()) {
+            return codesInCoding;
+        }
+
+        else return null;
     }
 
-
-    // RuntimeResourceDefinition definition = fhirContext.getResourceDefinition("ValueSet");
-	// BaseRuntimeChildDefinition childByName = definition.getChildByName("ValueSet.compose.include");
-	// childByName.getAccessor().getValues(valueSet);
-    private static ArrayList<Code> getDSTU3CodesFromCodeableConceptsObject(Object codes) {
-        ArrayList<Code> elmCodes = new ArrayList<Code>();
-        if (codes instanceof Iterable) {
-            for (Object codeObj : (Iterable) codes) {
-                elmCodes.addAll(getSTU3CodesFromCodeableConcept((org.hl7.fhir.dstu3.model.CodeableConcept)codeObj));
+    private static List<Code> getCodesInIterable(FhirContext fhirContext, Object object) {
+        List<Code> codes = new ArrayList<Code>();
+        try {
+            for (Object concept : (Iterable)object) {
+                codes.addAll(getCodesInConcept(fhirContext, concept));
             }
-        } else if (codes instanceof org.hl7.fhir.dstu3.model.CodeableConcept) {
-            elmCodes.addAll(getSTU3CodesFromCodeableConcept((org.hl7.fhir.dstu3.model.CodeableConcept)codes));
+        } catch (Exception e) {
+            //TODO: handle exception
         }
-        else if (codes instanceof org.hl7.fhir.dstu3.model.Coding) {
-            org.hl7.fhir.dstu3.model.Coding code = (org.hl7.fhir.dstu3.model.Coding) codes;
-            elmCodes.add(new Code().withCode(code.getCode()).withSystem(code.getSystem()));
-        }
-        return elmCodes;
+        return codes;
     }
 
-    private static ArrayList<Code> getSTU3CodesFromCodeableConcept(org.hl7.fhir.dstu3.model.CodeableConcept codeableConcept) {
-        ArrayList<Code> elmCodes = new ArrayList<Code>();
-        Iterable<org.hl7.fhir.dstu3.model.Coding> conceptCodes = codeableConcept.getCoding();
-        for (org.hl7.fhir.dstu3.model.Coding code : conceptCodes) {
-            elmCodes.add(new Code().withCode(code.getCode()).withSystem(code.getSystem()));
+    private static List<Code> getCodesInCoding(FhirContext fhirContext, Object object) {
+        //would like to get the coding element definition from the codingObject rather than hardcoding it here
+        RuntimeCompositeDatatypeDefinition codingDefinition = (RuntimeCompositeDatatypeDefinition)getElementDefinition(fhirContext, "Coding");
+        List<IBase> codingObjects = getCodingObjectsFromDefinition(codingDefinition, object);
+        if (codingObjects == null) {
+            return null;
         }
-        return elmCodes;
+
+        BaseRuntimeChildDefinition versionDefinition = (BaseRuntimeChildDefinition)codingDefinition.getChildByName("version");
+        BaseRuntimeChildDefinition codeDefinition = (BaseRuntimeChildDefinition)codingDefinition.getChildByName("code");
+        BaseRuntimeChildDefinition systemDefinition = (BaseRuntimeChildDefinition)codingDefinition.getChildByName("system");
+        BaseRuntimeChildDefinition displayDefinition = (BaseRuntimeChildDefinition)codingDefinition.getChildByName("display");
+
+
+        List<Code> codes = new ArrayList<>();
+        for (IBase coding : codingObjects) {
+            String code = getStringValueFromPrimitiveDefinition(coding, codeDefinition);
+            String display = getStringValueFromPrimitiveDefinition(coding, displayDefinition);
+            String system = getStringValueFromPrimitiveDefinition(coding, systemDefinition);
+			String version = getStringValueFromPrimitiveDefinition(coding, versionDefinition);
+				codes.add(new Code()
+					.withSystem(system)
+					.withCode(code)
+					.withDisplay(display)
+					.withVersion(version));
+        }
+        return codes;
     }
 
-    private static ArrayList<Code> getR4CodesFromCodeableConceptsObject(Object codes) {
-        ArrayList<Code> elmCodes = new ArrayList<Code>();
-        if (codes instanceof Iterable) {
-            for (Object codeObj : (Iterable) codes) {
-                Iterable<org.hl7.fhir.r4.model.Coding> conceptCodes = ((org.hl7.fhir.r4.model.CodeableConcept) codeObj).getCoding();
-                for (org.hl7.fhir.r4.model.Coding code : conceptCodes) {
-                    elmCodes.add(new Code().withCode(code.getCode()).withSystem(code.getSystem()));
-                }
-            }
-        } else if (codes instanceof org.hl7.fhir.r4.model.CodeableConcept) {
-            Iterable<org.hl7.fhir.r4.model.Coding> conceptCodes = ((org.hl7.fhir.r4.model.CodeableConcept) codes).getCoding();
-            for (org.hl7.fhir.r4.model.Coding code : conceptCodes) {
-                elmCodes.add(new Code().withCode(code.getCode()).withSystem(code.getSystem()));
-            }
+    private static List<Code> getCodesInConcept(FhirContext fhirContext, Object object) {
+        RuntimeCompositeDatatypeDefinition conceptDefinition = (RuntimeCompositeDatatypeDefinition)getElementDefinition(fhirContext, "CodeableConcept");
+        List<IBase> codingObjects = getCodingObjectsFromDefinition(conceptDefinition, object);
+        if(codingObjects == null) {
+            return null;
         }
-        else if (codes instanceof org.hl7.fhir.r4.model.Coding) {
-            org.hl7.fhir.r4.model.Coding code = (org.hl7.fhir.r4.model.Coding) codes;
-            elmCodes.add(new Code().withCode(code.getCode()).withSystem(code.getSystem()));
+        //would like to get the coding element definition from the codingObject rather than hardcoding it here
+        RuntimeCompositeDatatypeDefinition codingDefinition = (RuntimeCompositeDatatypeDefinition)getElementDefinition(fhirContext, "Coding");
+        
+        BaseRuntimeChildDefinition versionDefinition = (BaseRuntimeChildDefinition)codingDefinition.getChildByName("version");
+        BaseRuntimeChildDefinition codeDefinition = (BaseRuntimeChildDefinition)codingDefinition.getChildByName("code");
+        BaseRuntimeChildDefinition systemDefinition = (BaseRuntimeChildDefinition)codingDefinition.getChildByName("system");
+        BaseRuntimeChildDefinition displayDefinition = (BaseRuntimeChildDefinition)codingDefinition.getChildByName("display");
+
+
+        List<Code> codes = new ArrayList<>();
+        for (IBase coding : codingObjects) {
+            String code = getStringValueFromPrimitiveDefinition(coding, codeDefinition);
+            String display = getStringValueFromPrimitiveDefinition(coding, displayDefinition);
+            String system = getStringValueFromPrimitiveDefinition(coding, systemDefinition);
+			String version = getStringValueFromPrimitiveDefinition(coding, versionDefinition);
+				codes.add(new Code()
+					.withSystem(system)
+					.withCode(code)
+					.withDisplay(display)
+					.withVersion(version));
         }
-        return elmCodes;
+        return codes;
     }
+
+    private static BaseRuntimeElementDefinition getElementDefinition(FhirContext fhirContext, String ElementName) {
+        BaseRuntimeElementDefinition<?> def = fhirContext.getElementDefinition(ElementName);
+        return def;
+    }
+
+    private static List<IBase> getCodingObjectsFromDefinition(RuntimeCompositeDatatypeDefinition definition, Object object) {
+        BaseRuntimeChildDefinition coding = (BaseRuntimeChildDefinition)definition.getChildByName("coding");
+        List<IBase> codingObject = null;
+        try {
+            codingObject = coding.getAccessor().getValues(object);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        return codingObject;
+    }
+
+    private static String getStringValueFromPrimitiveDefinition(Object value, BaseRuntimeChildDefinition definition) {
+        IAccessor accessor = definition.getAccessor();
+		if (value == null || accessor == null) {
+			return null;
+		}
+
+		List<IBase> values = accessor.getValues(value);
+		if (values == null || values.isEmpty()) {
+			return null;
+		}
+
+		if (values.size() > 1) {
+			throw new IllegalArgumentException("More than one value returned while attempting to access primitive value.");
+		}
+
+		IBase baseValue = values.get(0);
+
+		if (!(baseValue instanceof IPrimitiveType)) {
+			throw new IllegalArgumentException("Non-primitive value encountered while trying to access primitive value.");
+		}
+		else {
+			return ((IPrimitiveType)baseValue).getValueAsString();
+		}
+	}
 
         
 }

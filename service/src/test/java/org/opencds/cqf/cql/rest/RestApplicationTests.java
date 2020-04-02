fb09877657097dee7f5081a7ce75bdb94d02e945
Jonathan Percival <jonathan.i.percival@gmail.com>
2020-04-02 13:34:21 -0600

    Remove measure eval temporarily

---
+++ service/src/test/java/org/opencds/cqf/cql/rest/RestApplicationTests.java
@@ -0,0 +1,16 @@
+package org.opencds.cqf.cql.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

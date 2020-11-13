831fff63c20df54ea0e7a6ae536d4de01f7f35b1
Jonathan Percival <jonathan.i.percival@gmail.com>
2020-11-13 12:37:38 -0700

    Fix script comments, add threads

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
-# Pass the current branch name as the first parameter of this function
-# e.g. ./scripts/build.sh
-
 # Fail if any command fails or if there are unbound variables and check syntax
 set -euxo pipefail
 bash -n "$0"
 
 # Run tests
-CMD="mvn test -B"
+CMD="mvn test -T 4 -B"
 if [[ "$TRAVIS_BRANCH" =~ master* ]]; then CMD="$CMD -P release"; fi
 eval $CMD
 
 
 # Run Deploy
-CMD="mvn deploy -DskipTests=true"
+CMD="mvn deploy -DskipTests=true -T 4 -B"
 
 # Import maven settings
 cp .travis.settings.xml $HOME/.m2/settings.xml
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-mvn install -U -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+mvn install -U -DskipTests=true -Dmaven.javadoc.skip=true -T 4 -B -V

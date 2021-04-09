16174065c08d1aa6c02619cfd3d37687a301fbd0
Jreyno77 <jreysteven1@gmail.com>
2021-04-09 15:35:42 -0600

    Added ActivityDefinitionModule

---
+++ evaluator.activitydefinition/src/test/java/org/opencds/cqf/cql/evaluator/library/ActivityDefinitionProcessorTests.java
@@ -0,0 +1,8 @@
+package org.opencds.cqf.cql.evaluator.library;
+
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
+
+public class ActivityDefinitionProcessorTests {
+
+}

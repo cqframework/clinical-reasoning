658c131fea0177352279987c5881f303ab4b54e9
Jreyno77 <jreysteven1@gmail.com>
2021-04-12 10:14:51 -0600

    Removed unused imports

--- evaluator.activitydefinition/src/test/java/org/opencds/cqf/cql/evaluator/library/ActivityDefinitionProcessorTests.java
+++ evaluator.activitydefinition/src/test/java/org/opencds/cqf/cql/evaluator/library/ActivityDefinitionProcessorTests.java
@@ -1,8 +1,5 @@
 package org.opencds.cqf.cql.evaluator.library;
 
-import static org.testng.Assert.assertTrue;
-import org.testng.annotations.Test;
-
 public class ActivityDefinitionProcessorTests {
 
 }

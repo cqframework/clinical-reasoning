82d51d2710513f502ea7298689d097308487fa4a
Jonathan Percival <jonathan.i.percival@gmail.com>
2021-10-07 12:48:39 -0600

    Updates to DateHelper

--- evaluator.measure/src/main/java/org/opencds/cqf/cql/evaluator/measure/helper/DateHelper.java
+++ evaluator.measure/src/main/java/org/opencds/cqf/cql/evaluator/measure/helper/DateHelper.java
@@ -2,7 +2,12 @@ package org.opencds.cqf.cql.evaluator.measure.helper;
 
 import java.text.DateFormat;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -11,30 +16,38 @@ import java.util.TimeZone;
 
 import joptsimple.internal.Strings;
 
+/**
+ * Helper class to resolve measurement period start and end dates. If a timezone
+ * is specified in a datetime, it's used. If not the timezone of the local
+ * system is used.
+ */
 public class DateHelper {
-
-    // Helper class to resolve period dates
     public static Date resolveRequestDate(String date, boolean start) {
+        // ISO Instance Format
+        if (date.contains("Z")) {
+            return Date.from(Instant.from(DateTimeFormatter.ISO_INSTANT.parse(date)));
+        }
+        // ISO Offset Format
         boolean isOffsetDateString = date.contains("T") && date.contains(".") && date.contains("-");
         if (isOffsetDateString) {
             return Date.from(Instant.from(DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(date)));
         }
-        // split it up - support dashes or slashes
-        String dateString = date;
-        String timeZoneString = "";
+
+        // Local DateTime
         if (date.contains("T")) {
-            dateString = date.substring(0, date.indexOf("T"));
-            timeZoneString = date.substring(date.indexOf("T") + 1, date.length());
+            TemporalAccessor ta = DateTimeFormatter.ISO_LOCAL_DATE_TIME.parse(date);
+            LocalDateTime ld = LocalDateTime.from(ta);
+            return Date.from(ld.atZone(ZoneId.systemDefault()).toInstant());
         }
-        Calendar calendar = Calendar.getInstance();
-        calendar.clear();
-        resolveDate(start, dateString, calendar);
-        resolveTime(timeZoneString, calendar);
-        return calendar.getTime();
+
+        return resolveDate(start, date);
     }
 
-    private static void resolveDate(boolean start, String dateString, Calendar calendar) {
-        String[] dissect = dateString.contains("-") ? dateString.split("-") : dateString.split("/");
+    private static Date resolveDate(boolean start, String dateString) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+
+        String[] dissect = dateString.split("-");
         List<Integer> dateVals = new ArrayList<>();
         for (String dateElement : dissect) {
             dateVals.add(Integer.parseInt(dateElement));
@@ -44,38 +57,51 @@ public class DateHelper {
             throw new IllegalArgumentException("Invalid date");
         }
 
-        // for now support dates up to day precision
         calendar.setTimeZone(TimeZone.getDefault());
+
+        // Set year
         calendar.set(Calendar.YEAR, dateVals.get(0));
+
+        // Set month (if defined, otherwise earliest if start, latest if end)
         if (dateVals.size() > 1) {
-            // java.util.Date months are zero based, hence the negative 1 -- 2014-01 == February 2014
+            // java.util.Date months are zero based, hence the negative 1 -- 2014-01 ==
+            // February 2014
             calendar.set(Calendar.MONTH, dateVals.get(1) - 1);
+        } else {
+            if (start) {
+                calendar.set(Calendar.MONTH, 0);
+            } else {
+                calendar.set(Calendar.MONTH, 11);
+            }
         }
+
+        // Set day (if defined, otherwise earliest if start, latest if end)
         if (dateVals.size() > 2)
             calendar.set(Calendar.DAY_OF_MONTH, dateVals.get(2));
         else {
             if (start) {
                 calendar.set(Calendar.DAY_OF_MONTH, 1);
-            }
-            else {
+            } else {
                 // get last day of month for end period
                 calendar.add(Calendar.MONTH, 1);
                 calendar.set(Calendar.DAY_OF_MONTH, 1);
                 calendar.add(Calendar.DATE, -1);
             }
         }
-    }
 
-    private static void resolveTime(String timeZoneString, Calendar calendar) {
-        if (!Strings.isNullOrEmpty(timeZoneString)) {
-            String[] dissectTimeZone = timeZoneString.split(":");
-            List<Integer> timeVals = new ArrayList<>();
-            for (String timeElement : dissectTimeZone) {
-                timeVals.add(Integer.parseInt(timeElement));
-            }
-            calendar.set(Calendar.HOUR_OF_DAY, timeVals.get(0));
-            calendar.set(Calendar.MINUTE, timeVals.get(1));
-            calendar.set(Calendar.SECOND, timeVals.get(2));
+        // Set time (not defined if we got this far. Earliest if start, latest if end)
+        if (start) {
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.MILLISECOND, 0);
+        } else {
+            calendar.set(Calendar.HOUR_OF_DAY, 23);
+            calendar.set(Calendar.MINUTE, 59);
+            calendar.set(Calendar.SECOND, 59);
+            calendar.set(Calendar.MILLISECOND, 999);
         }
+
+        return Date.from(calendar.toInstant());
     }
 }

a25d287240d5e03b5dfc961700d0dd3bab3c4267
Vitor Pamplona <vitor@vitorpamplona.com>
2022-06-06 21:33:00 -0400

    Reformat tests

--- evaluator.plandefinition/src/test/java/org/opencds/cqf/cql/evaluator/plandefinition/r4/PlanDefinitionProcessorTests.java
+++ evaluator.plandefinition/src/test/java/org/opencds/cqf/cql/evaluator/plandefinition/r4/PlanDefinitionProcessorTests.java
@@ -4,50 +4,50 @@ import org.testng.annotations.Test;
 
 public class PlanDefinitionProcessorTests extends PlanDefinitionProcessorTestBase {
     @Test
-    public void testHelloWorld() {
+    public void testChildRoutineVisit() {
         test(
-            "hello-world/hello-world-patient-data.json", 
-            "hello-world/hello-world-patient-view-bundle.json", 
-            "hello-world-patient-view", 
-            "helloworld-patient-1", 
-            "helloworld-patient-1-encounter-1", 
-            "hello-world/hello-world-careplan.json"
+                "child-routine-visit/child_routine_visit_patient.json",
+                "child-routine-visit/child_routine_visit_plan_definition.json",
+                "ChildRoutineVisit-PlanDefinition-1.0.0",
+                "ChildRoutine-Reportable",
+                "ChildRoutine-Encouter-Reportable",
+                "child-routine-visit/child_routine_visit_careplan.json"
         );
     }
 
     @Test
-    public void testRuleFiltersReportable() {
+    public void testHelloWorld() {
         test(
-            "tests-Reportable-bundle.json", 
-            "RuleFilters-1.0.0-bundle.json", 
-            "plandefinition-RuleFilters-1.0.0", 
-            "Reportable", 
-            null, 
-            "ReportableCarePlan.json"
+                "hello-world/hello-world-patient-data.json",
+                "hello-world/hello-world-patient-view-bundle.json",
+                "hello-world-patient-view",
+                "helloworld-patient-1",
+                "helloworld-patient-1-encounter-1",
+                "hello-world/hello-world-careplan.json"
         );
     }
 
     @Test
     public void testRuleFiltersNotReportable() {
         test(
-            "tests-NotReportable-bundle.json", 
-            "RuleFilters-1.0.0-bundle.json", 
-            "plandefinition-RuleFilters-1.0.0", 
-            "NotReportable", 
-            null, 
-            "NotReportableCarePlan.json"
+                "tests-NotReportable-bundle.json",
+                "RuleFilters-1.0.0-bundle.json",
+                "plandefinition-RuleFilters-1.0.0",
+                "NotReportable",
+                null,
+                "NotReportableCarePlan.json"
         );
     }
 
     @Test
-    public void testChildRoutineVisit() {
+    public void testRuleFiltersReportable() {
         test(
-            "child-routine-visit/child_routine_visit_patient.json", 
-            "child-routine-visit/child_routine_visit_plan_definition.json", 
-            "ChildRoutineVisit-PlanDefinition-1.0.0", 
-            "ChildRoutine-Reportable", 
-            "ChildRoutine-Encouter-Reportable", 
-            "child-routine-visit/child_routine_visit_careplan.json"
+                "tests-Reportable-bundle.json",
+                "RuleFilters-1.0.0-bundle.json",
+                "plandefinition-RuleFilters-1.0.0",
+                "Reportable",
+                null,
+                "ReportableCarePlan.json"
         );
     }
 }

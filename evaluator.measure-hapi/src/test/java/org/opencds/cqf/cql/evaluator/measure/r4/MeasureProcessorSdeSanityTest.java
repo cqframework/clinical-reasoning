647d1c7cb22523b8f55574df90ff4ae2972e4a96
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2022-07-14 08:18:29 -0600

    rectify test name

--- evaluator.measure-hapi/src/test/java/org/opencds/cqf/cql/evaluator/measure/r4/MeasureProcessorSdeSanityTest.java
+++ evaluator.measure-hapi/src/test/java/org/opencds/cqf/cql/evaluator/measure/r4/MeasureProcessorSdeSanityTest.java
@@ -13,7 +13,7 @@ public class MeasureProcessorSdeSanityTest extends BaseMeasureProcessorTest {
     }
 
     @Test
-    public void exm124_subject_list() {
+    public void measure_eval_unique_extension_list() {
         MeasureReport report = this.measureProcessor.evaluateMeasure("http://cds.optum.com/dqm-diabetes/fhir/Measure/DM1Measure",
                 "2020-01-01", "2022-06-29", "subject",
                 "Patient/DM1-patient-1", null, null,

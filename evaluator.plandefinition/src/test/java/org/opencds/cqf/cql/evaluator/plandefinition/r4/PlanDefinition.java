1ea21324611795bfbfae434928e703432ba9e0ee
c-schuler <hoofschu@gmail.com>
2022-11-21 13:45:09 -0700

    Added R4 test for multiple actions

--- evaluator.plandefinition/src/test/java/org/opencds/cqf/cql/evaluator/plandefinition/r4/PlanDefinition.java
+++ evaluator.plandefinition/src/test/java/org/opencds/cqf/cql/evaluator/plandefinition/r4/PlanDefinition.java
@@ -273,7 +273,6 @@ public class PlanDefinition {
         }
 
         public void isEqualsTo(String expectedCarePlanAssetName) {
-            String s = jsonParser.encodeResourceToString(carePlan);
             try {
                 JSONAssert.assertEquals(
                         load(expectedCarePlanAssetName),

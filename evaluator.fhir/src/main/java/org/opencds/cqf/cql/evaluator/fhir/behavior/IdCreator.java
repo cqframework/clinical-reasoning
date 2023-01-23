4da9ed9b1644108e5fee99bc0e99d7e36621440f
Chalma Maadaadi <chalma@alphora.com>
2023-01-23 08:00:12 -0500

    Integrating care-gaps common code.

---
+++ evaluator.fhir/src/main/java/org/opencds/cqf/cql/evaluator/fhir/behavior/IdCreator.java
@@ -0,0 +1,22 @@
+package org.opencds.cqf.cql.evaluator.fhir.behavior;
+
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.opencds.cqf.cql.evaluator.fhir.util.Ids;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public interface IdCreator extends FhirContextUser {
+
+	default <T extends IIdType> T newId(String theResourceName, String theResourceId) {
+		checkNotNull(theResourceName);
+		checkNotNull(theResourceId);
+
+		return Ids.newId(getFhirContext(), theResourceName, theResourceId);
+	}
+
+	default <T extends IIdType> T newId(String theResourceId) {
+		checkNotNull(theResourceId);
+
+		return Ids.newId(getFhirContext(), theResourceId);
+	}
+}

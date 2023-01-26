6d0cade371d567728e608b24729080ed74ade6ea
mdnazmulkarim <nazmul.karim@smilecdr.com>
2023-01-26 10:41:09 -0700

    Feature cqf setup : add test repository creation utilities (#222)
    * add test data
    
    * BundleFhirRepository wip
    
    * add test utility
    
    * update test data
    
    * load test data test
    
    * R4MeasureProcessor constructor
    
    * update Repository api changes, develop R4TestRepository
    
    * add tests for R4TestRepository
    
    * relocate fhirRepository
    
    * relocate BundleFhirRepository
    
    * import fix
    
    * use utility for new Bundle creation
    
    * update fhir repository searchByurl

---
+++ evaluator.fhir/src/main/java/org/opencds/cqf/cql/evaluator/fhir/util/ResourceLoader.java
@@ -0,0 +1,76 @@
+package org.opencds.cqf.cql.evaluator.fhir.util;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import org.apache.commons.io.IOUtils;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public interface ResourceLoader {
+
+    default Object loadBundle(FhirContext context, String theLocation) {
+        IBaseBundle resource = (IBaseBundle) readResource(context, theLocation);
+        return resource;
+    }
+
+    default IBaseResource readResource(FhirContext context, String theLocation) {
+        String resourceString = stringFromResource(theLocation);
+        if (theLocation.endsWith("json")) {
+            return parseResource(context, "json", resourceString);
+        } else {
+            return parseResource(context,"xml", resourceString);
+        }
+    }
+
+    public default IBaseResource parseResource(FhirContext context, String encoding, String resourceString) {
+        IParser parser;
+        switch (encoding.toLowerCase()) {
+            case "json":
+                parser = context.newJsonParser();
+                break;
+            case "xml":
+                parser = context.newXmlParser();
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        String.format("Expected encoding xml, or json.  %s is not a valid encoding", encoding));
+        }
+
+        return parser.parseResource(resourceString);
+    }
+
+    default IBaseResource loadResource(FhirContext context, String theLocation) {
+        String resourceString = stringFromResource(theLocation);
+        if (theLocation.endsWith("json")) {
+            return loadResource(context,"json", resourceString);
+        } else {
+            return loadResource(context,"xml", resourceString);
+        }
+    }
+
+    default IBaseResource loadResource(FhirContext context, String encoding, String resourceString) {
+        IBaseResource resource = parseResource(context, encoding, resourceString);
+        return resource;
+    }
+
+    @SuppressWarnings("java:S112")
+    default String stringFromResource(String theLocation) {
+        InputStream is = null;
+        try {
+            if (theLocation.startsWith(File.separator)) {
+                is = new FileInputStream(theLocation);
+            } else {
+                is = ResourceLoader.class.getResourceAsStream(theLocation);
+            }
+            return IOUtils.toString(is, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Error loading resource from %s", theLocation), e);
+        }
+
+    }
+}
---
+++ evaluator.measure-hapi/src/test/java/org/opencds/cqf/cql/evaluator/measure/r4/R4RepositoryTest.java
@@ -0,0 +1,49 @@
+package org.opencds.cqf.cql.evaluator.measure.r4;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Patient;
+import org.opencds.cqf.cql.evaluator.fhir.repository.r4.FhirRepository;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+
+public class R4RepositoryTest {
+
+    FhirRepository repository;
+
+    public R4RepositoryTest() {
+        repository = new FhirRepository(this.getClass(),
+                List.of("res/tests", "res/vocabulary/CodeSystem/", "res/vocabulary/ValueSet/", "res/content/"), false);
+    }
+
+    @Test
+    public void testRead() {
+        IBaseResource res = repository.read(Patient.class, new IdType("example"), null);
+        assertEquals(res.getIdElement().getIdPart(), "example");
+    }
+
+
+    @Test
+    public void testCreate() {
+        Patient john = new Patient();
+        john.setId(new IdType("Patient", "id-john-doe"));
+        MethodOutcome methodOutcome = repository.create(john, null);
+        assertTrue(methodOutcome.getCreated());
+
+        repository.create(john, null);
+    }
+
+    @Test
+    public void testSearch() {
+        IBaseBundle bundle = repository.search(Bundle.class, Patient.class, null, null);
+        assertEquals(((Bundle) bundle).getEntry().size(), 3);
+    }
+}

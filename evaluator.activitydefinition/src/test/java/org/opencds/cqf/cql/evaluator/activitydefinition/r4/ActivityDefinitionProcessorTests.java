23d3fc0fabde129b5f772bff5e7a30476ec7c2b8
Brenin Rhodes <brenin@alphora.com>
2023-03-02 13:30:28 -0700

    R4 ActivityDefinition tests passing

--- evaluator.activitydefinition/src/test/java/org/opencds/cqf/cql/evaluator/activitydefinition/r4/ActivityDefinitionProcessorTests.java
+++ evaluator.activitydefinition/src/test/java/org/opencds/cqf/cql/evaluator/activitydefinition/r4/ActivityDefinitionProcessorTests.java
@@ -1,13 +1,14 @@
 package org.opencds.cqf.cql.evaluator.activitydefinition.r4;
 
+import java.util.List;
+
 import org.hl7.fhir.exceptions.FHIRException;
-import org.hl7.fhir.r4.model.Coding;
-import org.hl7.fhir.r4.model.Endpoint;
-import org.hl7.fhir.r4.model.Endpoint.EndpointStatus;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.MedicationRequest;
-import org.opencds.cqf.cql.evaluator.fhir.Constants;
 import org.opencds.cqf.cql.evaluator.fhir.dal.FhirDal;
+import org.opencds.cqf.cql.evaluator.fhir.repository.r4.FhirRepository;
+import org.opencds.cqf.cql.evaluator.fhir.util.Repositories;
+import org.opencds.cqf.cql.evaluator.library.LibraryEngine;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -28,21 +29,32 @@ public class ActivityDefinitionProcessorTests {
 
   @Test
   public void testActivityDefinitionApply() throws FHIRException {
-    Endpoint contentEndpoint = new Endpoint().setStatus(EndpointStatus.ACTIVE)
-        .setAddress("bundle-activityDefinitionTest.json")
-        .setConnectionType(new Coding().setCode(Constants.HL7_FHIR_FILES));
+    // Endpoint contentEndpoint = new Endpoint().setStatus(EndpointStatus.ACTIVE)
+    // .setAddress("bundle-activityDefinitionTest.json")
+    // .setConnectionType(new Coding().setCode(Constants.HL7_FHIR_FILES));
 
-    Endpoint terminologyEndpoint = new Endpoint().setStatus(EndpointStatus.ACTIVE)
-        .setAddress("bundle-activityDefinitionTest.json")
-        .setConnectionType(new Coding().setCode(Constants.HL7_FHIR_FILES));
+    // Endpoint terminologyEndpoint = new Endpoint().setStatus(EndpointStatus.ACTIVE)
+    // .setAddress("bundle-activityDefinitionTest.json")
+    // .setConnectionType(new Coding().setCode(Constants.HL7_FHIR_FILES));
 
-    Endpoint dataEndpoint = new Endpoint().setStatus(EndpointStatus.ACTIVE)
-        .setAddress("bundle-activityDefinitionTest.json")
-        .setConnectionType(new Coding().setCode(Constants.HL7_FHIR_FILES));
+    // Endpoint dataEndpoint = new Endpoint().setStatus(EndpointStatus.ACTIVE)
+    // .setAddress("bundle-activityDefinitionTest.json")
+    // .setConnectionType(new Coding().setCode(Constants.HL7_FHIR_FILES));
+
+    // Object result = this.activityDefinitionProcessor.apply(new IdType("activityDefinition-test"),
+    // "patient-1", null, null, null, null, null, null, null, null, null, contentEndpoint,
+    // terminologyEndpoint, dataEndpoint);
+
+    FhirRepository data = new FhirRepository(this.getClass(), List.of("res/tests"), false);
+    FhirRepository content = new FhirRepository(this.getClass(), List.of("res/content/"), false);
+    FhirRepository terminology = new FhirRepository(this.getClass(),
+        List.of("res/vocabulary/CodeSystem/", "res/vocabulary/ValueSet/"), false);
+
+    var repository = Repositories.proxy(data, content, terminology);
+    var libraryEngine = new LibraryEngine(fhirContext, repository);
 
     Object result = this.activityDefinitionProcessor.apply(new IdType("activityDefinition-test"),
-        "patient-1", null, null, null, null, null, null, null, null, null, contentEndpoint,
-        terminologyEndpoint, dataEndpoint);
+        "patient-1", null, null, null, null, null, null, null, null, null, libraryEngine);
     Assert.assertTrue(result instanceof MedicationRequest);
     MedicationRequest request = (MedicationRequest) result;
     Assert.assertTrue(request.getDoNotPerform());

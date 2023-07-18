d3dafe7bac3af188fcda72a85585a2911e805c00
Brenin Rhodes <brenin@alphora.com>
2023-07-18 11:05:53 -0600

    Instantiate a single ModelResolver in the ResourceMatchers (#324)
    * Instantiate a single ModelResolver in the ResourceMatchers
    
    * Wrap resolvers in a caching decorator

--- evaluator.fhir/src/main/java/org/opencds/cqf/cql/evaluator/fhir/repository/matcher/ResourceMatcherR4.java
+++ evaluator.fhir/src/main/java/org/opencds/cqf/cql/evaluator/fhir/repository/matcher/ResourceMatcherR4.java
@@ -13,15 +13,19 @@ import org.hl7.fhir.r4.model.Period;
 import org.hl7.fhir.r4.model.Timing;
 import org.opencds.cqf.cql.engine.fhir.model.R4FhirModelResolver;
 import org.opencds.cqf.cql.engine.model.ModelResolver;
+import org.opencds.cqf.cql.evaluator.engine.model.CachingModelResolverDecorator;
 
 import ca.uhn.fhir.model.base.composite.BaseCodingDt;
 import ca.uhn.fhir.rest.param.DateRangeParam;
 import ca.uhn.fhir.rest.param.InternalCodingDt;
 
 public class ResourceMatcherR4 implements BaseResourceMatcher {
+  private final ModelResolver modelResolver =
+      new CachingModelResolverDecorator(new R4FhirModelResolver());
+
   @Override
   public ModelResolver getModelResolver() {
-    return new R4FhirModelResolver();
+    return modelResolver;
   }
 
   @Override

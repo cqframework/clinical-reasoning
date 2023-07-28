0991e4225e350a23b2e0f0c2e6a44fdfc92d900b
Brenin Rhodes <brenin@alphora.com>
2023-07-28 14:58:23 -0600

    Add support for paging repository search results

--- evaluator.fhir/src/main/java/org/opencds/cqf/cql/evaluator/fhir/util/dstu3/SearchHelper.java
+++ evaluator.fhir/src/main/java/org/opencds/cqf/cql/evaluator/fhir/util/dstu3/SearchHelper.java
@@ -1,12 +1,19 @@
 package org.opencds.cqf.cql.evaluator.fhir.util.dstu3;
 
+import java.util.List;
+import java.util.Map;
+
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Resource;
 import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IPrimitiveType;
 import org.opencds.cqf.cql.evaluator.fhir.util.Canonicals;
 import org.opencds.cqf.fhir.api.Repository;
 import org.opencds.cqf.fhir.utility.Searches;
+
+import ca.uhn.fhir.model.api.IQueryParameterType;
 
 public class SearchHelper {
 
@@ -29,5 +36,26 @@ public class SearchHelper {
     }
 
     return searchResult.getEntryFirstRep().getResource();
+  }
+
+  public static <T extends IBaseResource> Bundle searchRepositoryWithPaging(Repository repository,
+      Class<T> resourceType, Map<String, List<IQueryParameterType>> searchParameters,
+      Map<String, String> headers) {
+    var result = repository.search(Bundle.class, resourceType, searchParameters, headers);
+    var next = result.getLink(IBaseBundle.LINK_NEXT);
+    if (next != null) {
+      getNextPage(repository, result, next.getUrl());
+    }
+
+    return result;
+  }
+
+  private static void getNextPage(Repository repository, Bundle bundle, String nextUrl) {
+    var nextBundle = repository.link(Bundle.class, nextUrl);
+    nextBundle.getEntry().forEach(bundle::addEntry);
+    var next = nextBundle.getLink(IBaseBundle.LINK_NEXT);
+    if (next != null) {
+      getNextPage(repository, bundle, next.getUrl());
+    }
   }
 }

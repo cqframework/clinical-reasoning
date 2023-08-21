725e58eda2da2ac1d90cf7171037080a9b88c2ba
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-08-21 12:24:40 -0600

    WIP

--- evaluator.spring/src/main/java/org/opencds/cqf/cql/evaluator/spring/EvaluatorConfiguration.java
+++ evaluator.spring/src/main/java/org/opencds/cqf/cql/evaluator/spring/EvaluatorConfiguration.java
@@ -2,7 +2,6 @@ package org.opencds.cqf.cql.evaluator.spring;
 
 import org.opencds.cqf.cql.evaluator.spring.builder.BuilderConfiguration;
 import org.opencds.cqf.cql.evaluator.spring.cql2elm.Cql2ElmConfiguration;
-import org.opencds.cqf.cql.evaluator.spring.expression.ExpressionConfiguration;
 import org.opencds.cqf.cql.evaluator.spring.fhir.FhirConfiguration;
 import org.opencds.cqf.cql.evaluator.spring.library.LibraryConfiguration;
 import org.opencds.cqf.cql.evaluator.spring.measure.MeasureConfiguration;
@@ -10,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ExpressionConfiguration.class, LibraryConfiguration.class, Cql2ElmConfiguration.class,
+@Import({LibraryConfiguration.class, Cql2ElmConfiguration.class,
     FhirConfiguration.class, BuilderConfiguration.class, MeasureConfiguration.class})
 public class EvaluatorConfiguration {
 

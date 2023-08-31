ca5030d4fee12477cc6e8fac349d8d64fba91d47
Brenin Rhodes <brenin@alphora.com>
2023-08-31 16:33:29 -0600

    cleanup

--- evaluator.spring/src/main/java/org/opencds/cqf/cql/evaluator/spring/EvaluatorConfiguration.java
+++ evaluator.spring/src/main/java/org/opencds/cqf/cql/evaluator/spring/EvaluatorConfiguration.java
@@ -2,13 +2,12 @@ package org.opencds.cqf.cql.evaluator.spring;
 
 import org.opencds.cqf.cql.evaluator.spring.cql2elm.Cql2ElmConfiguration;
 import org.opencds.cqf.cql.evaluator.spring.fhir.FhirConfiguration;
-import org.opencds.cqf.cql.evaluator.spring.library.LibraryConfiguration;
 import org.opencds.cqf.cql.evaluator.spring.measure.MeasureConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({LibraryConfiguration.class, Cql2ElmConfiguration.class,
+@Import({Cql2ElmConfiguration.class,
     FhirConfiguration.class, MeasureConfiguration.class})
 public class EvaluatorConfiguration {
 

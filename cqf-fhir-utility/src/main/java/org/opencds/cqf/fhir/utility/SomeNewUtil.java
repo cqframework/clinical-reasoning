35735452388622fbc94c526732403f7ed3a2585e
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-11-19 12:16:48 -0700

    Should fail

---
+++ cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/SomeNewUtil.java
@@ -0,0 +1,16 @@
+package org.opencds.cqf.fhir.utility;
+
+public class SomeNewUtil {
+
+    public void thing() {
+        System.out.println("Hello, world!");
+    }
+
+    public void thing2() {
+        System.out.println("Hello, thing2!");
+    }
+
+    public void thing3() {
+        System.out.println("Hello, thing2!");
+    }
+}

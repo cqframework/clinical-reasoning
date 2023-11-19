eb2e1ddd83942b966165fe656132df956ef864ae
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-11-19 12:41:41 -0700

    Should now pass

---
+++ cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/SomeNewUtilTest.java
@@ -0,0 +1,14 @@
+package org.opencds.cqf.fhir.utility;
+
+import org.junit.jupiter.api.Test;
+
+public class SomeNewUtilTest {
+
+    @Test
+    public void test() {
+        SomeNewUtil util = new SomeNewUtil();
+        util.thing();
+        util.thing2();
+        util.thing3();
+    }
+}

acbe2cf3b83104ee065116f43a46cc7e369efab5
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-12-21 11:19:50 -0700

    Update to fhir core 6.2.6

--- cqf-fhir-cr-cli/src/main/java/org/opencds/cqf/fhir/cr/cli/command/CqlCommand.java
+++ cqf-fhir-cr-cli/src/main/java/org/opencds/cqf/fhir/cr/cli/command/CqlCommand.java
@@ -11,11 +11,11 @@ import org.cqframework.cql.cql2elm.CqlTranslatorOptions;
 import org.cqframework.cql.cql2elm.CqlTranslatorOptionsMapper;
 import org.cqframework.fhir.npm.NpmProcessor;
 import org.cqframework.fhir.utilities.IGContext;
+import org.cqframework.fhir.utilities.LoggerAdapter;
 import org.hl7.elm.r1.VersionedIdentifier;
 import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.instance.model.api.IBaseDatatype;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r5.context.IWorkerContext;
 import org.opencds.cqf.cql.engine.execution.EvaluationResult;
 import org.opencds.cqf.cql.engine.execution.ExpressionResult;
 import org.opencds.cqf.fhir.cql.CqlOptions;
@@ -23,6 +23,7 @@ import org.opencds.cqf.fhir.cql.Engines;
 import org.opencds.cqf.fhir.cql.EvaluationSettings;
 import org.opencds.cqf.fhir.utility.repository.IGFileStructureRepository;
 import org.opencds.cqf.fhir.utility.repository.IGLayoutMode;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
@@ -30,49 +31,45 @@ import picocli.CommandLine.Option;
 
 @Command(name = "cql", mixinStandardHelpOptions = true)
 public class CqlCommand implements Callable<Integer> {
-    @Option(
-            names = {"-fv", "--fhir-version"},
-            required = true)
+    private static final Logger logger = LoggerFactory.getLogger(CqlCommand.class);
+
+    @Option(names = { "-fv", "--fhir-version" }, required = true)
     public String fhirVersion;
 
-    @Option(names = {"-op", "--options-path"})
+    @Option(names = { "-op", "--options-path" })
     public String optionsPath;
 
     @ArgGroup(multiplicity = "0..1", exclusive = false)
     public NamespaceParameter namespace;
 
     static class NamespaceParameter {
-        @Option(names = {"-nn", "--namespace-name"})
+        @Option(names = { "-nn", "--namespace-name" })
         public String namespaceName;
 
-        @Option(names = {"-nu", "--namespace-uri"})
+        @Option(names = { "-nu", "--namespace-uri" })
         public String namespaceUri;
     }
 
-    @Option(names = {"-rd", "--root-dir"})
+    @Option(names = { "-rd", "--root-dir" })
     public String rootDir;
 
-    @Option(names = {"-ig", "--ig-path"})
+    @Option(names = { "-ig", "--ig-path" })
     public String igPath;
 
     @ArgGroup(multiplicity = "1..*", exclusive = false)
     List<LibraryParameter> libraries;
 
     static class LibraryParameter {
-        @Option(
-                names = {"-lu", "--library-url"},
-                required = true)
+        @Option(names = { "-lu", "--library-url" }, required = true)
         public String libraryUrl;
 
-        @Option(
-                names = {"-ln", "--library-name"},
-                required = true)
+        @Option(names = { "-ln", "--library-name" }, required = true)
         public String libraryName;
 
-        @Option(names = {"-lv", "--library-version"})
+        @Option(names = { "-lv", "--library-version" })
         public String libraryVersion;
 
-        @Option(names = {"-t", "--terminology-url"})
+        @Option(names = { "-t", "--terminology-url" })
         public String terminologyUrl;
 
         @ArgGroup(multiplicity = "0..1", exclusive = false)
@@ -81,54 +78,34 @@ public class CqlCommand implements Callable<Integer> {
         @ArgGroup(multiplicity = "0..*", exclusive = false)
         public List<ParameterParameter> parameters;
 
-        @Option(names = {"-e", "--expression"})
+        @Option(names = { "-e", "--expression" })
         public String[] expression;
 
         @ArgGroup(multiplicity = "0..1", exclusive = false)
         public ContextParameter context;
 
         static class ContextParameter {
-            @Option(names = {"-c", "--context"})
+            @Option(names = { "-c", "--context" })
             public String contextName;
 
-            @Option(names = {"-cv", "--context-value"})
+            @Option(names = { "-cv", "--context-value" })
             public String contextValue;
         }
 
         static class ModelParameter {
-            @Option(names = {"-m", "--model"})
+            @Option(names = { "-m", "--model" })
             public String modelName;
 
-            @Option(names = {"-mu", "--model-url"})
+            @Option(names = { "-mu", "--model-url" })
             public String modelUrl;
         }
 
         static class ParameterParameter {
-            @Option(names = {"-p", "--parameter"})
+            @Option(names = { "-p", "--parameter" })
             public String parameterName;
 
-            @Option(names = {"-pv", "--parameter-value"})
+            @Option(names = { "-pv", "--parameter-value" })
             public String parameterValue;
-        }
-    }
-
-    private class Logger implements IWorkerContext.ILoggingService {
-
-        private final org.slf4j.Logger log = LoggerFactory.getLogger(Logger.class);
-
-        @Override
-        public void logMessage(String s) {
-            log.warn(s);
-        }
-
-        @Override
-        public void logDebugMessage(LogCategory logCategory, String s) {
-            log.debug("{}: {}", logCategory, s);
-        }
-
-        @Override
-        public boolean isDebugLogging() {
-            return log.isDebugEnabled();
         }
     }
 
@@ -154,7 +131,7 @@ public class CqlCommand implements Callable<Integer> {
 
         IGContext igContext = null;
         if (rootDir != null && igPath != null) {
-            igContext = new IGContext(new Logger());
+            igContext = new IGContext(new LoggerAdapter(logger));
             igContext.initializeFromIg(rootDir, igPath, toVersionNumber(fhirVersionEnum));
         }
 
@@ -222,9 +199,9 @@ public class CqlCommand implements Callable<Integer> {
             IBaseResource resource = (IBaseResource) value;
             result = resource.fhirType()
                     + (resource.getIdElement() != null
-                                    && resource.getIdElement().hasIdPart()
-                            ? "(id=" + resource.getIdElement().getIdPart() + ")"
-                            : "");
+                            && resource.getIdElement().hasIdPart()
+                                    ? "(id=" + resource.getIdElement().getIdPart() + ")"
+                                    : "");
         } else if (value instanceof IBase) {
             result = ((IBase) value).fhirType();
         } else if (value instanceof IBaseDatatype) {

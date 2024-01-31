c84fd092ea6bce7498e1916fcb787d9415ddd029
Brenin Rhodes <brenin@alphora.com>
2024-01-31 09:24:03 -0700

    fix test

--- cqf-fhir-cr/src/test/java/org/opencds/cqf/fhir/cr/plandefinition/apply/ProcessGoalTests.java
+++ cqf-fhir-cr/src/test/java/org/opencds/cqf/fhir/cr/plandefinition/apply/ProcessGoalTests.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.opencds.cqf.fhir.cr.helpers.RequestHelpers.newPDApplyRequestForVersion;
 
 import ca.uhn.fhir.context.FhirVersionEnum;
+import org.hl7.fhir.dstu3.model.Coding;
 import org.junit.jupiter.api.Test;
 
 public class ProcessGoalTests {
@@ -23,7 +24,8 @@ public class ProcessGoalTests {
     void testConvertDstu3Goal() {
         var request = newPDApplyRequestForVersion(FhirVersionEnum.DSTU3);
         var goalElement = new org.hl7.fhir.dstu3.model.PlanDefinition.PlanDefinitionGoalComponent()
-                .addTarget(new org.hl7.fhir.dstu3.model.PlanDefinition.PlanDefinitionGoalTargetComponent());
+                .addTarget(new org.hl7.fhir.dstu3.model.PlanDefinition.PlanDefinitionGoalTargetComponent()
+                        .setDetail(new org.hl7.fhir.dstu3.model.CodeableConcept(new Coding("test", "test", "test"))));
         var result = fixture.convertGoal(request, goalElement);
         assertTrue(result instanceof org.hl7.fhir.dstu3.model.Goal);
         var goal = (org.hl7.fhir.dstu3.model.Goal) result;

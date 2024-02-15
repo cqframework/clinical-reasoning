5dc10d1305831dcf2fde1bdbf90316c467907503
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-02-15 09:56:14 -0700

    Fix casing errors

---
+++ cqf-fhir-test/src/test/java/org/opencds/cqf/fhir/test/ResourcesTest.java
@@ -0,0 +1,14 @@
+package org.opencds.cqf.fhir.test;
+
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class ResourcesTest {
+
+    @TempDir
+    static Path tempDir;
+
+    @Test
+    void copyFromJar() {}
+}

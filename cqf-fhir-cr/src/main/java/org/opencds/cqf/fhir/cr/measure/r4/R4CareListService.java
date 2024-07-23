fd28cdd755adc8ef23061a832609934bc25197ed
Justin McKelvy <60718638+Capt-Mac@users.noreply.github.com>
2024-07-23 10:27:00 -0600

    Create R4CareListService.java

---
+++ cqf-fhir-cr/src/main/java/org/opencds/cqf/fhir/cr/measure/r4/R4CareListService.java
@@ -0,0 +1,19 @@
+package org.opencds.cqf.fhir.cr.measure.r4;
+
+public class R4CareListService {
+    //get subjects
+    //get Measure(s)
+    //validate Measure has care-gap extension
+    //check for Date of non-compliance
+    //check measureScoringType compliance
+    //ratio/proportion algorithm
+    //prospective logic
+    //create bundle for results
+    //add DetectedIssue per Subject/per Measure
+    //set date of non-compliance extension
+    //set status of care-gap
+    //set evidence by Measure resource
+    //set dates to measurement period used
+
+
+}

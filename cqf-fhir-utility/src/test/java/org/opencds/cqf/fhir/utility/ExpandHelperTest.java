89f62262a011c188ade32e5ace2902f00a2cfdf2
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-08-21 16:44:12 -0400

    add test for expansion params

--- cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/ExpandHelperTest.java
+++ cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/ExpandHelperTest.java
@@ -1,6 +1,9 @@
 package org.opencds.cqf.fhir.utility;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -13,14 +16,18 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.FhirVersionEnum;
 import java.util.ArrayList;
 import java.util.Optional;
+import org.hl7.fhir.instance.model.api.IPrimitiveType;
 import org.hl7.fhir.r4.model.CanonicalType;
 import org.hl7.fhir.r4.model.Endpoint;
 import org.hl7.fhir.r4.model.Parameters;
+import org.hl7.fhir.r4.model.Parameters.ParametersParameterComponent;
 import org.hl7.fhir.r4.model.UriType;
 import org.hl7.fhir.r4.model.ValueSet;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.opencds.cqf.fhir.api.Repository;
 import org.opencds.cqf.fhir.utility.adapter.AdapterFactory;
+import org.opencds.cqf.fhir.utility.adapter.ParametersAdapter;
 import org.opencds.cqf.fhir.utility.adapter.ValueSetAdapter;
 import org.opencds.cqf.fhir.utility.client.TerminologyServerClient;
 
@@ -90,6 +97,57 @@ public class ExpandHelperTest {
         verify(rep, never()).search(any(), any(), any());
         verify(client, times(1)).getResource(any(), any(), any());
         verify(client, times(1)).expand(any(ValueSetAdapter.class), any(), any());
+    }
+
+    @Test
+    void expandGrouperUpdatesURLAndVersionExpParamsTest() {
+        // setup tx server endpoint
+        var baseUrl = "www.test.com/fhir";
+        var endpoint = new Endpoint();
+        endpoint.setAddress(baseUrl);
+        // setup Vsets
+        var leafUrl = baseUrl + "/ValueSet/leaf";
+        // ensure that the grouper is not expanded using the Tx Server
+        var grouperUrl = "www.different-base-url.com/fhir/ValueSet/grouper";
+        var grouperVersion = "1.9.2";
+        var grouper = new ValueSet();
+        grouper.setUrl(grouperUrl);
+        grouper.setVersion(grouperVersion);
+        grouper.getCompose().getIncludeFirstRep().getValueSet().add(new CanonicalType(leafUrl));
+        grouper.addExtension().setUrl(Constants.AUTHORITATIVE_SOURCE_URL).setValue(new UriType(grouperUrl));
+        var leaf = createLeafWithUrl(leafUrl);
+
+        // shouldn't be used
+        var rep = mockRepositoryWithValueSetR4(leaf);
+
+        // should be used
+        var client = mockTerminologyServerWithValueSetR4(leaf);
+
+        var expandHelper = new ExpandHelper(rep.fhirContext(), client);
+        var expansionParams = new Parameters();
+        // Setup exp params with Grouper URL and version
+        expansionParams.addParameter(TerminologyServerClient.urlParamName, grouperUrl);
+        expansionParams.addParameter(TerminologyServerClient.versionParamName, grouperVersion);
+        expandHelper.expandValueSet(
+                (ValueSetAdapter) this.factory.createKnowledgeArtifactAdapter(grouper),
+                factory.createParameters(expansionParams),
+                // important part of the test
+                Optional.of(factory.createEndpoint(endpoint)),
+                new ArrayList<ValueSetAdapter>(),
+                new ArrayList<String>(),
+                rep);
+        var parametersCaptor = ArgumentCaptor.forClass(ParametersAdapter.class);
+        verify(client, times(1)).expand(any(ValueSetAdapter.class), any(), parametersCaptor.capture());
+        var childExpParams = parametersCaptor.getValue();
+        assertNotNull(childExpParams.getParameter(TerminologyServerClient.urlParamName));
+        // leaf is expanded with Leaf url not Grouper url
+        assertTrue(((IPrimitiveType<String>) ((ParametersParameterComponent)
+                                childExpParams.getParameter(TerminologyServerClient.urlParamName))
+                        .getValue())
+                .getValue()
+                .equals(leafUrl));
+        // the Version parameter is removed because leaf has no version
+        assertNull(childExpParams.getParameter(TerminologyServerClient.versionParamName));
     }
 
     ValueSet createLeafWithUrl(String url) {

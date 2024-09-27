c2c5bb8159f02d6987ab9931db10496ad2ea6401
Ivan Baisi <ivan.baisi@smiledigitalhealth.com>
2024-09-27 09:20:02 -0700

    format code

--- cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/visitor/AbstractKnowledgeArtifactVisitor.java
+++ cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/visitor/AbstractKnowledgeArtifactVisitor.java
@@ -47,10 +47,11 @@ public abstract class AbstractKnowledgeArtifactVisitor implements IKnowledgeArti
             Optional<KnowledgeArtifactAdapter> maybeArtifact =
                     VisitorHelper.tryGetLatestVersion(preReleaseReference, repository);
             if (maybeArtifact.isPresent()) {
-                if (resourcesToUpdate.stream().noneMatch(rtu ->
-                    rtu.getId().equals(maybeArtifact.get().getId().toString())
-                        && (rtu.getExtension().stream()
-                        .anyMatch(ext -> ext.getUrl().equals(isOwnedUrl))))) {
+                if (resourcesToUpdate.stream()
+                        .noneMatch(rtu ->
+                                rtu.getId().equals(maybeArtifact.get().getId().toString())
+                                        && (rtu.getExtension().stream()
+                                                .anyMatch(ext -> ext.getUrl().equals(isOwnedUrl))))) {
                     resourcesToUpdate.add(maybeArtifact.get().get());
                     getComponents(maybeArtifact.get(), repository, resourcesToUpdate);
                 }

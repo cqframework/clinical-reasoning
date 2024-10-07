eaec5c14a68fbd820a14afd0df8f6adc6424b6ce
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-10-07 14:50:28 -0600

    More try tests

--- cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/monad/TryTest.java
+++ cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/monad/TryTest.java
@@ -2,6 +2,7 @@ package org.opencds.cqf.fhir.utility.monad;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -30,5 +31,36 @@ class TryTest {
         assertFalse(result.hasResult());
         assertTrue(result.hasException());
         assertEquals(e, result.exception());
+
+        t = Tries.of(1);
+        result = t.flatMap(i -> {
+            throw new RuntimeException("runtime exception");
+        });
+        assertFalse(result.hasResult());
+        assertTrue(result.hasException());
+        assertInstanceOf(RuntimeException.class, result.exception());
+    }
+
+    @Test
+    void map() {
+        var t = Tries.of(1);
+        var result = t.map(i -> i + 1);
+        assertTrue(result.hasResult());
+        assertEquals(2, result.getOrThrow());
+
+        var e = new Exception();
+        t = Tries.<Integer>ofException(e);
+        result = t.map(i -> i + 1);
+        assertFalse(result.hasResult());
+        assertTrue(result.hasException());
+        assertEquals(e, result.exception());
+
+        t = Tries.of(1);
+        result = t.map(i -> {
+            throw new RuntimeException("runtime exception");
+        });
+        assertFalse(result.hasResult());
+        assertTrue(result.hasException());
+        assertInstanceOf(RuntimeException.class, result.exception());
     }
 }

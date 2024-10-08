40a94ec51862800ecfeb36f039387cb18f5efaa8
Taha <TahaAttari@users.noreply.github.com>
2024-10-07 21:35:23 -0400

    Semver sort and tests (#546)
    * [APHL-1201] semver sort and tests
    
    * update coverage
    
    * reduce sonar issues
    
    * reduce complexity
    
    * less complexity
    
    * complexity
    
    * fix tests
    
    ---------
    
    Co-authored-by: taha.attari@smilecdr.com <taha.attari@smilecdr.com>

--- cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/Versions.java
+++ cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/Versions.java
@@ -2,8 +2,10 @@ package org.opencds.cqf.fhir.utility;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import org.apache.commons.lang3.tuple.Pair;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
 /**
@@ -21,16 +23,15 @@ public class Versions {
      */
     public static int compareVersions(String version1, String version2) {
         // Treat null as MAX VERSION
-        if (version1 == null && version2 == null) {
-            return 0;
+        if (version1 == null || version2 == null) {
+            return handleNulls(version1, version2);
         }
 
-        if (version1 != null && version2 == null) {
-            return -1;
-        }
+        final var v1Valid = isValidSemver(version1);
+        final var v2Valid = isValidSemver(version2);
 
-        if (version1 == null) {
-            return 1;
+        if (!v1Valid || !v2Valid) {
+            return handleInvalids(version1, version2, v1Valid, v2Valid);
         }
 
         String[] string1Vals = version1.split("\\.");
@@ -39,21 +40,128 @@ public class Versions {
         int length = Math.max(string1Vals.length, string2Vals.length);
 
         for (int i = 0; i < length; i++) {
-            Integer v1 = i < string1Vals.length ? Integer.parseInt(string1Vals[i]) : 0;
-            Integer v2 = i < string2Vals.length ? Integer.parseInt(string2Vals[i]) : 0;
-
-            // Making sure Version1 bigger than version2
-            if (v1 > v2) {
-                return 1;
-            }
-            // Making sure Version1 smaller than version2
-            else if (v1 < v2) {
+            if (i > string1Vals.length - 1) {
                 return -1;
             }
+            if (i > string2Vals.length - 1) {
+                return 1;
+            }
+            if (i == length - 1) {
+                break;
+            }
+            if (!string1Vals[i].equals(string2Vals[i])) {
+                return stringOrNumberCompare(string1Vals[i], string2Vals[i]);
+            }
         }
+        final var tail1 = parseTail(string1Vals[length - 1]);
+        final var tail2 = parseTail(string2Vals[length - 1]);
 
-        // Both are equal
-        return 0;
+        if (tail1.getLeft().equals(tail2.getLeft())) {
+            return compareTails(tail1, tail2);
+        } else {
+            return intCompare(tail1.getLeft(), tail2.getLeft());
+        }
+    }
+
+    private static Integer compareTails(Pair<Integer, String> tail1, Pair<Integer, String> tail2) {
+        if (!tail1.getRight().isEmpty() && tail2.getRight().isEmpty()) {
+            return 1;
+        } else if (tail1.getRight().isEmpty() && !tail2.getRight().isEmpty()) {
+            return -1;
+        } else {
+            final var c = tail1.getRight().compareTo(tail2.getRight());
+            // compareTo returns numbers outside [-1,1]
+            if (c > 0) {
+                return 1;
+            } else if (c < 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    private static Integer handleNulls(String version1, String version2) {
+        if (version1 == null && version2 == null) {
+            return 0;
+        } else if (version1 != null && version2 == null) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    private static Integer handleInvalids(String version1, String version2, boolean v1Valid, boolean v2Valid) {
+        if (!v1Valid && !v2Valid) {
+            return stringOrNumberCompare(version1, version2);
+        } else if (v1Valid && !v2Valid) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    private static Integer stringOrNumberCompare(String version1, String version2) {
+        // try string and number compares if it's not semver
+        try {
+            final var d1 = Integer.parseInt(version1);
+            final var d2 = Integer.parseInt(version2);
+            return intCompare(d1, d2);
+        } catch (NumberFormatException e) {
+            final var c = version1.compareTo(version2);
+            // compareTo returns numbers outside [-1,1]
+            if (c > 0) {
+                return 1;
+            } else if (c < 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    private static Integer intCompare(Integer d1, Integer d2) {
+        if (d1 > d2) {
+            return 1;
+        } else if (d2 > d1) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static boolean isValidSemver(String check) {
+        if (check.length() > 1 && !check.contains(".")) {
+            return false;
+        }
+        String[] stringVals = check.split("\\.");
+        for (var i = 0; i < stringVals.length - 1; i++) {
+            try {
+                Integer.parseInt(stringVals[i]);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        try {
+            parseTail(stringVals[stringVals.length - 1]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static Pair<Integer, String> parseTail(String tail) {
+        try {
+            return Pair.of(Integer.parseInt(tail), "");
+        } catch (NumberFormatException e) {
+            if (tail.contains("-")) {
+                final var splitDash = tail.split("-");
+                final var afterDash = String.join("-", Arrays.copyOfRange(splitDash, 1, splitDash.length));
+                return Pair.of(Integer.parseInt(splitDash[0]), afterDash);
+            } else {
+                throw e;
+            }
+        }
     }
 
     /***

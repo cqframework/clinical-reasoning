99d3e75203d6d197e386ac0d0596576c43b57f85
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-10-15 11:02:30 -0600

    WIP for tests

--- cqf-fhir-cql/src/test/java/org/opencds/cqf/fhir/cql/EnginesTest.java
+++ cqf-fhir-cql/src/test/java/org/opencds/cqf/fhir/cql/EnginesTest.java
@@ -1,15 +1,24 @@
 package org.opencds.cqf.fhir.cql;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import ca.uhn.fhir.context.FhirContext;
+import org.cqframework.fhir.npm.NpmProcessor;
+import org.cqframework.fhir.utilities.IGContext;
+import org.cqframework.fhir.utilities.LoggerAdapter;
 import org.junit.jupiter.api.Test;
 import org.opencds.cqf.fhir.api.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class EnginesTest {
+
+    private static Logger log = LoggerFactory.getLogger(EnginesTest.class);
 
     @Test
     void debugLoggingEnabled() {
@@ -17,7 +26,7 @@ class EnginesTest {
         when(repository.fhirContext()).thenReturn(FhirContext.forR4Cached());
         var settings = EvaluationSettings.getDefault();
         settings.getCqlOptions().getCqlEngineOptions().setDebugLoggingEnabled(true);
-        var engine = Engines.forRepository(repository, settings, null);
+        var engine = Engines.forRepository(repository, settings);
         assertTrue(engine.getState().getDebugMap().getIsLoggingEnabled());
     }
 
@@ -27,7 +36,28 @@ class EnginesTest {
         when(repository.fhirContext()).thenReturn(FhirContext.forR4Cached());
         var settings = EvaluationSettings.getDefault();
         settings.getCqlOptions().getCqlEngineOptions().setDebugLoggingEnabled(false);
-        var engine = Engines.forRepository(repository, settings, null);
+        var engine = Engines.forRepository(repository, settings);
         assertNull(engine.getState().getDebugMap());
+    }
+
+    @Test
+    void npmProcessor() {
+        var repository = mock(Repository.class);
+        when(repository.fhirContext()).thenReturn(FhirContext.forR4Cached());
+
+        var igContext = new IGContext(new LoggerAdapter(log));
+        igContext.initializeFromIni("todo.ini");
+        var settings = EvaluationSettings
+                .getDefault()
+                .toBuilder()
+                .npmProcessor(new NpmProcessor(igContext))
+                .build();
+
+        var engine = Engines.forRepository(repository, settings);
+        var lm = engine.getEnvironment().getLibraryManager();
+
+        var ni = lm.getNamespaceManager().getNamespaceInfoFromUri("");
+        assertNotNull(ni);
+        assertEquals("FHIR", ni.getName());
     }
 }

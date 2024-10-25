0e1641ca5cb62bd92ad5fbc03901ae9146ab71f7
Chris O Riordan <chris.oriordan@smilecdr.com>
2024-10-25 12:44:19 -0700

    Cleanup Delete, Withdraw, Retire Visitor Tests & Data

--- cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/visitor/r4/RetireVisitorTest.java
+++ cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/visitor/r4/RetireVisitorTest.java
@@ -1,6 +1,7 @@
 package org.opencds.cqf.fhir.utility.visitor.r4;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.opencds.cqf.fhir.utility.r4.Parameters.parameters;
 import static org.opencds.cqf.fhir.utility.r4.Parameters.part;
 
@@ -43,11 +44,11 @@ public class RetireVisitorTest {
     @Test
     void library_retire_test() {
         Bundle bundle =
-                (Bundle) jsonParser.parseResource(WithdrawVisitorTests.class.getResourceAsStream("Bundle-retire.json"));
+                (Bundle) jsonParser.parseResource(WithdrawVisitorTests.class.getResourceAsStream("Bundle-ersd-small-active.json"));
         Bundle tsBundle = repo.transaction(bundle);
         // Resource is uploaded using POST - need to get id like this
         String id = tsBundle.getEntry().get(0).getResponse().getLocation();
-        String version = "1.1.0";
+        String version = "2022-11-10";
         Library library = repo.read(Library.class, new IdType(id)).copy();
         ILibraryAdapter libraryAdapter = new AdapterFactory().createLibrary(library);
         IKnowledgeArtifactVisitor retireVisitor = new RetireVisitor();
@@ -56,7 +57,7 @@ public class RetireVisitorTest {
 
         var res = returnedBundle.getEntry();
 
-        assert (res.size() == 9);
+        assert (res.size() == 4);
         var libraries = repo.search(Bundle.class, Library.class, Map.of()).getEntry().stream()
                 .filter(x -> ((Library) x.getResource()).getStatus().equals(Enumerations.PublicationStatus.RETIRED))
                 .collect(Collectors.toList());
@@ -71,27 +72,23 @@ public class RetireVisitorTest {
                 .collect(Collectors.toList());
 
         assert (libraries.size() == 2);
-        assert (valueSets.size() == 6);
+        assert (valueSets.size() == 1);
         assert (planDefinitions.size() == 1);
     }
 
     @Test
     void library_retire_no_draft_test() {
-        try {
-            Bundle bundle = (Bundle) jsonParser.parseResource(
-                    WithdrawVisitorTests.class.getResourceAsStream("Bundle-small-approved-draft.json"));
-            repo.transaction(bundle);
-            String version = "1.01.21";
-            Library library = repo.read(Library.class, new IdType("Library/SpecificationLibrary"))
-                    .copy();
-            ILibraryAdapter libraryAdapter = new AdapterFactory().createLibrary(library);
-            IKnowledgeArtifactVisitor retireVisitor = new RetireVisitor();
-            Parameters params = parameters(part("version", version));
-            libraryAdapter.accept(retireVisitor, repo, params);
+        Bundle bundle = (Bundle) jsonParser.parseResource(
+                WithdrawVisitorTests.class.getResourceAsStream("Bundle-small-approved-draft.json"));
+        repo.transaction(bundle);
+        String version = "1.01.21";
+        Library library = repo.read(Library.class, new IdType("Library/SpecificationLibrary"))
+                .copy();
+        ILibraryAdapter libraryAdapter = new AdapterFactory().createLibrary(library);
+        IKnowledgeArtifactVisitor retireVisitor = new RetireVisitor();
+        Parameters params = parameters(part("version", version));
 
-            fail("Trying to withdraw an active Library should throw an Exception");
-        } catch (PreconditionFailedException e) {
-            assert (e.getMessage().contains("Cannot retire an artifact that is not in active status"));
-        }
+        var exception = assertThrows(PreconditionFailedException.class, () -> libraryAdapter.accept(retireVisitor, repo, params));
+        assertTrue(exception.getMessage().contains("Cannot retire an artifact that is not in active status"));
     }
 }

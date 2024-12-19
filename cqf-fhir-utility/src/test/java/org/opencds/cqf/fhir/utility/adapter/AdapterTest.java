33d94425c87215eb52c0b2ed1ae252870c13aa3d
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-12-18 19:27:28 -0500

    spotless

--- cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/adapter/AdapterTest.java
+++ cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/adapter/AdapterTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertThrows;
 import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import java.util.Date;
-
 import org.hl7.fhir.r4.model.Library;
 import org.junit.jupiter.api.Test;
 
@@ -21,11 +20,12 @@ class AdapterTest {
         assertThrows(UnprocessableEntityException.class, () -> IAdapter.newDateType(version, date));
         assertThrows(UnprocessableEntityException.class, () -> IAdapter.newDateTimeType(version, date));
     }
+
     @Test
     void testResolvPath() {
         var library = new Library();
         library.setDate(new Date());
         var adapter = IAdapterFactory.createAdapterForResource(library);
-        assertThrows(UnprocessableEntityException.class, () -> adapter.resolvePathString(library,"date"));
+        assertThrows(UnprocessableEntityException.class, () -> adapter.resolvePathString(library, "date"));
     }
 }

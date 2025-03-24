d410718ec9a321c4d2af19ce943382ef06c0ce95
Brenin Rhodes <brenin.rhodes@smiledigitalhealth.com>
2025-03-24 14:34:15 -0600

    sonar

--- cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/client/ExpandRunner.java
+++ cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/client/ExpandRunner.java
@@ -64,6 +64,8 @@ public class ExpandRunner implements Runnable {
                         terminologyServerClientSettings.getTimeoutSeconds()));
             }
         } catch (ExecutionException | InterruptedException e) {
+            scheduler.shutdown();
+            Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
     }

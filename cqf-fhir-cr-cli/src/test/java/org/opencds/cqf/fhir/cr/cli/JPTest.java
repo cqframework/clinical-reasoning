353c87ebf9533e193c3180158739bc365853c602
Jonathan Percival <jonathan.i.percival@gmail.com>
2025-04-21 10:56:21 -0600

    More tidying

--- cqf-fhir-cr-cli/src/test/java/org/opencds/cqf/fhir/cr/cli/JPTest.java
+++ cqf-fhir-cr-cli/src/test/java/org/opencds/cqf/fhir/cr/cli/JPTest.java
@@ -1,20 +1,20 @@
 package org.opencds.cqf.fhir.cr.cli;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class JPTest {
 
     @Test
-    void hedis() {
+    void hedis() throws IOException {
         run("AABReporting", "/Users/jp/hedis", 500);
     }
 
-    void run(String libraryName, String patientPath, int count) {
-        var baseArgs = List.of(
+    void run(String libraryName, String patientPath, int count) throws IOException {
+        var baseArgs = Stream.of(
                 "cql",
                 "-fv=R4",
                 "-rd=/Users/jp/repos/ncqa-hedis-discovery",
@@ -25,18 +25,17 @@ class JPTest {
                 "-mu=" + patientPath,
                 "-t=/Users/jp/repos/ncqa-hedis-discovery/input/vocabulary/valueset");
 
-        var patientArgs = allPatients(patientPath, count).stream()
-                .flatMap(id -> Stream.of("-c=Patient", "-cv=" + id))
-                .toList();
+        var patientArgs = allPatients(patientPath, count).flatMap(id -> Stream.of("-c=Patient", "-cv=" + id));
 
-        var args = Stream.concat(baseArgs.stream(), patientArgs.stream()).toArray(String[]::new);
+        var args = Stream.concat(baseArgs, patientArgs).toArray(String[]::new);
         Main.run(args);
     }
 
-    List<String> allPatients(String dataPath, int count) {
-        return Stream.of(Path.of(dataPath, "tests/Patient").toFile().listFiles(File::isDirectory))
+    Stream<String> allPatients(String dataPath, int count) throws IOException {
+        return Files.list(Path.of(dataPath, "tests/Patient"))
+                .filter(Files::isDirectory)
                 .limit(count)
-                .map(File::getName)
-                .toList();
+                .map(Path::getFileName)
+                .map(Path::toString);
     }
 }

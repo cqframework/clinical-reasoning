232c36df455999858577980658d02fcfd68d6614
Jonathan Percival <jonathan.i.percival@gmail.com>
2025-08-26 10:20:32 -0700

    WIP

--- cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/repository/ig/CompartmentMode.java
+++ cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/repository/ig/CompartmentMode.java
@@ -1,12 +1,16 @@
 package org.opencds.cqf.fhir.utility.repository.ig;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.RuntimeSearchParam;
 import ca.uhn.fhir.rest.api.RestSearchParameterTypeEnum;
 import com.google.common.base.Supplier;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Table;
 import com.google.common.collect.Tables;
+
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Whether or not the files are organized by compartment. This is primarily used for tests to
@@ -20,13 +24,13 @@ public enum CompartmentMode {
     GROUP("Group"),
     NONE("");
 
-    // | Compartment Type | Resource Type | Is Member? |
-    // |------------------|---------------|------------|
-    // | Patient          | Encounter     | true       |
-    // | Patient          | Library       | false      |
-    private static Table<String, String, Boolean> compartmentMembershipCache =
+    // | Compartment Type | Resource Type | Candidate Search Params |
+    // |------------------|---------------|-------------------------|
+    // | Patient          | Encounter     | ["subject"]             |
+    // | Patient          | Library       | []                      |
+    private static Table<String, String, Set<RuntimeSearchParam>> compartmentMembershipCache =
             Tables.newCustomTable(Maps.newLinkedHashMap(), new Supplier<>() {
-                public Map<String, Boolean> get() {
+                public Map<String, Set<RuntimeSearchParam>> get() {
                     return Maps.newLinkedHashMap();
                 }
             });
@@ -50,23 +54,37 @@ public enum CompartmentMode {
         return NONE;
     }
 
-    public boolean resourceBelongsToCompartment(FhirContext fhirContext, String resourceName) {
-        if (this.type.equals(resourceName)) {
-            return true;
+    public boolean resourceBelongsToCompartment(FhirContext fhirContext, String resourceType) {
+        return !compartmentSearchParams(fhirContext, resourceType).isEmpty();
+    }
+
+    private Set<RuntimeSearchParam> compartmentSearchParams(FhirContext fhirContext, String resourceType) {
+        var compartmentMap = compartmentMembershipCache.row(this.type);
+        if (compartmentMap.containsKey(resourceType)) {
+            return compartmentMap.get(resourceType);
         }
 
-        var resourceMap = compartmentMembershipCache.row(this.type);
-        if (resourceMap.containsKey(resourceName)) {
-            return resourceMap.get(resourceName);
+        Set<RuntimeSearchParam> params;
+        if (this.type.equals(resourceType)) {
+            // If the compartment type is the same as the resource type, we can assume that the resource
+            // belongs to its own compartment.
+            params = fhirContext.getResourceDefinition(resourceType).getSearchParams().stream()
+                    .filter(param -> param.getName().equals("_id"))
+                    .collect(Collectors.toSet());
+            if (params.isEmpty()) {
+                // Should be impossible
+                throw new IllegalStateException("No _id search parameter found for resource type: " + resourceType);
+            }
         }
-
-        var belongs = fhirContext.getResourceDefinition(resourceName).getSearchParams().stream()
+        else {
+            params = fhirContext.getResourceDefinition(resourceType).getSearchParams().stream()
                 .filter(param -> param.getParamType() == RestSearchParameterTypeEnum.REFERENCE)
-                .anyMatch(param -> param.getProvidesMembershipInCompartments() != null
-                        && param.getProvidesMembershipInCompartments().contains(this.type));
+                .filter(param -> param.getProvidesMembershipInCompartments() != null
+                        && param.getProvidesMembershipInCompartments().contains(this.type)).collect(Collectors.toSet());
+        }
 
-        compartmentMembershipCache.put(this.type, resourceName, belongs);
 
-        return belongs;
+        compartmentMembershipCache.put(this.type, resourceType, params);
+        return params;
     }
 }

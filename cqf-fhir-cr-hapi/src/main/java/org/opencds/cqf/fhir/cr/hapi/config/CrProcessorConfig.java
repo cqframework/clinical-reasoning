275f30126872dbfa2df3971f88cbf91140434781
Etienne Poirier <33007955+epeartree@users.noreply.github.com>
2025-09-05 16:38:17 -0400

    Skeleton for supporting operation apply on a R4 GraphDefinition resource (#718)
    * Skeleton for supporting operation apply on a R4 GraphDefinition resource.
    
    * fixing checkstyle violation
    
    * basic test.
    
    * Add request object for apply and wire up package and datarequirements operations
    
    * spotless
    
    * Correctly handle non group items with children during Questionnaire population (#720)
    
    * correctly handle non group items with children during population
    
    * add test
    
    * Rev to 3.26.0
    
    * Fix docs warnings
    
    * Rev to 3.27.0-SNAPSHOT
    
    * Fixes bug in R4ReleaseVisitor that cause omission of some direct refe… (#723)
    
    * Fixes bug in R4ReleaseVisitor that causes the omission of some direct reference codes
    
    * CRMI Approve Operation (R4) (#721)
    
    * CRMI Approve Operation (R4)
    
    - Added $approve operation provider and service
    - Added the operation to the R4 CR config
    - Added documentation
    - Added tests
    
    * Small change to factory function name
    
    * Added test
    
    * CRMI Draft Operation (R4) (#722)
    
    * CRMI Draft Operation (R4)
    
    - Added $draft operation provider and service
    - Added the operation to the R4 CR config
    - Added documentation
    - Added tests
    
    * Added tests
    
    * Fix broken tests
    
    * Cleaning up merge conflicts
    
    * CRMI Release Operation (R4) (#725)
    
    - Added $release operation provider and service
    - Added the operation to the R4 CR config
    - Added documentation
    - Added tests
    
    * Moving utility class EndpointHelper from cr-hapi to cr-utility for reuse. (#734)
    
    * moving EndpointHelper from cr-hapi to cr-utility for reuse
    
    * spotless happiness
    
    * moving relevant test to utility package
    
    ---------
    
    Co-authored-by: peartree <etienne.poirier@smilecdr.com>
    
    * enhancement to basic test.
    
    * wip: adding ApplyRequest/ApplyRequestBuilder and factories
    
    * MVP with tests
    
    * spotless happiness
    
    * Add adapters for GraphDefinition
    
    * adding adapters and tests.
    
    * fix test
    
    ---------
    
    Co-authored-by: peartree <etienne.poirier@smilecdr.com>
    Co-authored-by: Brenin Rhodes <brenin.rhodes@smiledigitalhealth.com>
    Co-authored-by: Brenin Rhodes <brenin@alphora.com>
    Co-authored-by: Jonathan Percival <jonathan.i.percival@gmail.com>
    Co-authored-by: Adam Stevenson <adam.stevenson@smiledigitalhealth.com>
    Co-authored-by: c-schuler <hoofschu@gmail.com>

--- cqf-fhir-cr-hapi/src/main/java/org/opencds/cqf/fhir/cr/hapi/config/CrProcessorConfig.java
+++ cqf-fhir-cr-hapi/src/main/java/org/opencds/cqf/fhir/cr/hapi/config/CrProcessorConfig.java
@@ -3,7 +3,11 @@ package org.opencds.cqf.fhir.cr.hapi.config;
 import ca.uhn.fhir.rest.api.server.IRepositoryFactory;
 import org.opencds.cqf.fhir.cql.EvaluationSettings;
 import org.opencds.cqf.fhir.cr.activitydefinition.ActivityDefinitionProcessor;
+import org.opencds.cqf.fhir.cr.graphdefintion.GraphDefinitionProcessor;
+import org.opencds.cqf.fhir.cr.graphdefintion.apply.ApplyRequestBuilder;
 import org.opencds.cqf.fhir.cr.hapi.common.IActivityDefinitionProcessorFactory;
+import org.opencds.cqf.fhir.cr.hapi.common.IGraphDefinitionApplyRequestBuilderFactory;
+import org.opencds.cqf.fhir.cr.hapi.common.IGraphDefinitionProcessorFactory;
 import org.opencds.cqf.fhir.cr.hapi.common.ILibraryProcessorFactory;
 import org.opencds.cqf.fhir.cr.hapi.common.IPlanDefinitionProcessorFactory;
 import org.opencds.cqf.fhir.cr.hapi.common.IQuestionnaireProcessorFactory;
@@ -18,6 +22,7 @@ import org.opencds.cqf.fhir.utility.client.TerminologyServerClientSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SuppressWarnings("UnstableApiUsage")
 @Configuration
 public class CrProcessorConfig {
     @Bean
@@ -63,5 +68,21 @@ public class CrProcessorConfig {
             TerminologyServerClientSettings terminologyServerClientSettings) {
         return rd -> new ValueSetProcessor(
                 repositoryFactory.create(rd), evaluationSettings, terminologyServerClientSettings);
+    }
+
+    @Bean
+    IGraphDefinitionProcessorFactory graphDefinitionProcessorFactory(
+            IRepositoryFactory repositoryFactory,
+            EvaluationSettings evaluationSettings,
+            TerminologyServerClientSettings terminologyServerClientSettings) {
+        return rd -> new GraphDefinitionProcessor(
+                repositoryFactory.create(rd), evaluationSettings, terminologyServerClientSettings);
+    }
+
+    @Bean
+    IGraphDefinitionApplyRequestBuilderFactory graphDefinitionApplyRequestBuilderFactory(
+            IRepositoryFactory repositoryFactory, EvaluationSettings evaluationSettings) {
+
+        return rd -> new ApplyRequestBuilder(repositoryFactory.create(rd), evaluationSettings);
     }
 }
--- cqf-fhir-cr-hapi/src/main/java/org/opencds/cqf/fhir/cr/hapi/config/r4/ApplyOperationConfig.java
+++ cqf-fhir-cr-hapi/src/main/java/org/opencds/cqf/fhir/cr/hapi/config/r4/ApplyOperationConfig.java
@@ -1,16 +1,19 @@
 package org.opencds.cqf.fhir.cr.hapi.config.r4;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.rest.server.RestfulServer;
 import java.util.Arrays;
 import java.util.Map;
 import org.opencds.cqf.fhir.cr.hapi.common.IActivityDefinitionProcessorFactory;
+import org.opencds.cqf.fhir.cr.hapi.common.IGraphDefinitionApplyRequestBuilderFactory;
+import org.opencds.cqf.fhir.cr.hapi.common.IGraphDefinitionProcessorFactory;
 import org.opencds.cqf.fhir.cr.hapi.common.IPlanDefinitionProcessorFactory;
+import org.opencds.cqf.fhir.cr.hapi.common.StringTimePeriodHandler;
 import org.opencds.cqf.fhir.cr.hapi.config.CrProcessorConfig;
 import org.opencds.cqf.fhir.cr.hapi.config.ProviderLoader;
 import org.opencds.cqf.fhir.cr.hapi.config.ProviderSelector;
 import org.opencds.cqf.fhir.cr.hapi.r4.activitydefinition.ActivityDefinitionApplyProvider;
+import org.opencds.cqf.fhir.cr.hapi.r4.graphdefinition.GraphDefinitionApplyProvider;
 import org.opencds.cqf.fhir.cr.hapi.r4.plandefinition.PlanDefinitionApplyProvider;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -32,14 +35,30 @@ public class ApplyOperationConfig {
         return new PlanDefinitionApplyProvider(planDefinitionProcessorFactory);
     }
 
+    @Bean
+    GraphDefinitionApplyProvider r4GraphDefinitionApplyProvider(
+            IGraphDefinitionProcessorFactory graphDefinitionProcessorFactory,
+            IGraphDefinitionApplyRequestBuilderFactory graphDefinitionApplyRequestBuilderFactory,
+            FhirContext fhirContext,
+            StringTimePeriodHandler stringTimePeriodHandler) {
+        return new GraphDefinitionApplyProvider(
+                graphDefinitionProcessorFactory,
+                graphDefinitionApplyRequestBuilderFactory,
+                fhirContext.getVersion().getVersion(),
+                stringTimePeriodHandler);
+    }
+
     @Bean(name = "applyOperationLoader")
     public ProviderLoader applyOperationLoader(
             ApplicationContext applicationContext, FhirContext fhirContext, RestfulServer restfulServer) {
         var selector = new ProviderSelector(
                 fhirContext,
                 Map.of(
-                        FhirVersionEnum.R4,
-                        Arrays.asList(ActivityDefinitionApplyProvider.class, PlanDefinitionApplyProvider.class)));
+                        fhirContext.getVersion().getVersion(),
+                        Arrays.asList(
+                                ActivityDefinitionApplyProvider.class,
+                                PlanDefinitionApplyProvider.class,
+                                GraphDefinitionApplyProvider.class)));
 
         return new ProviderLoader(restfulServer, applicationContext, selector);
     }

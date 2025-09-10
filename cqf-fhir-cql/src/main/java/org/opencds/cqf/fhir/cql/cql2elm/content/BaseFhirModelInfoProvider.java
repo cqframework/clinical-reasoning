bcb9588f9be016c375167962a96426ada8a0e44e
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2025-09-10 13:54:00 -0600

    Add support for loading model info from repository Library resources

---
+++ cqf-fhir-cql/src/main/java/org/opencds/cqf/fhir/cql/cql2elm/content/BaseFhirModelInfoProvider.java
@@ -0,0 +1,68 @@
+package org.opencds.cqf.fhir.cql.cql2elm.content;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import jakarta.xml.bind.JAXB;
+import org.cqframework.cql.cql2elm.LibraryContentType;
+import org.cqframework.cql.cql2elm.LibrarySourceProvider;
+import org.hl7.cql.model.ModelIdentifier;
+import org.hl7.cql.model.ModelInfoProvider;
+import org.hl7.elm.r1.VersionedIdentifier;
+import org.hl7.elm_modelinfo.r1.ModelInfo;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.instance.model.api.ICompositeType;
+import org.opencds.cqf.fhir.utility.adapter.IAdapterFactory;
+import org.opencds.cqf.fhir.utility.adapter.IAttachmentAdapter;
+import org.opencds.cqf.fhir.utility.adapter.ILibraryAdapter;
+
+/**
+ * This class implements logic for extracting content from a FHIR Library resource and provides an
+ * extension point for implementing the fetch of a FHIR library matching a specific identifier.
+ */
+public abstract class BaseFhirModelInfoProvider implements ModelInfoProvider {
+
+    protected IAdapterFactory adapterFactory;
+
+    protected BaseFhirModelInfoProvider(IAdapterFactory adapterFactory) {
+        this.adapterFactory = requireNonNull(adapterFactory, "adapterFactory can not be null");
+    }
+
+    @Override
+    public ModelInfo load(ModelIdentifier modelIdentifier) {
+        var is = getModelInfoContent(modelIdentifier, ModelInfoContentType.XML);
+        return JAXB.unmarshal(is, ModelInfo.class);
+    }
+
+    protected InputStream getModelInfoContent(ModelIdentifier modelIdentifier, ModelInfoContentType modelInfoContentType) {
+        requireNonNull(modelIdentifier, "versionedIdentifier can not be null.");
+        requireNonNull(modelInfoContentType, "modelInfoContentType can not be null.");
+
+        IBaseResource library = this.getLibrary(modelIdentifier);
+        if (library == null) {
+            return null;
+        }
+
+        return this.getContentStream(library, modelInfoContentType.mimeType());
+    }
+
+    protected InputStream getContentStream(IBaseResource library, String contentType) {
+
+        ILibraryAdapter libraryAdapter = this.adapterFactory.createLibrary(library);
+
+        if (libraryAdapter.hasContent()) {
+            for (ICompositeType attachment : libraryAdapter.getContent()) {
+                IAttachmentAdapter attachmentAdapter = this.adapterFactory.createAttachment(attachment);
+                if (attachmentAdapter.getContentType().equals(contentType)) {
+                    // get externalized extension if present and add custom load data
+                    return new ByteArrayInputStream(attachmentAdapter.getData());
+                }
+            }
+        }
+
+        return null;
+    }
+
+    protected abstract IBaseResource getLibrary(ModelIdentifier libraryIdentifier);
+}
---
+++ cqf-fhir-cql/src/main/java/org/opencds/cqf/fhir/cql/cql2elm/content/RepositoryFhirModelInfoProvider.java
@@ -0,0 +1,72 @@
+package org.opencds.cqf.fhir.cql.cql2elm.content;
+
+import static java.util.Objects.requireNonNull;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.repository.IRepository;
+import java.util.ArrayList;
+import org.hl7.cql.model.ModelIdentifier;
+import org.hl7.elm.r1.VersionedIdentifier;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.opencds.cqf.fhir.cql.cql2elm.util.LibraryVersionSelector;
+import org.opencds.cqf.fhir.utility.adapter.IAdapterFactory;
+import org.opencds.cqf.fhir.utility.iterable.BundleIterable;
+import org.opencds.cqf.fhir.utility.search.Searches;
+
+public class RepositoryFhirModelInfoProvider extends BaseFhirModelInfoProvider {
+
+    private final IRepository repository;
+    private final FhirContext fhirContext;
+    private final LibraryVersionSelector libraryVersionSelector;
+
+    public RepositoryFhirModelInfoProvider(
+        IRepository repository, IAdapterFactory adapterFactory, LibraryVersionSelector libraryVersionSelector) {
+        super(adapterFactory);
+        this.repository = requireNonNull(repository, "repository can not be null");
+        this.fhirContext = repository.fhirContext();
+        this.libraryVersionSelector = requireNonNull(libraryVersionSelector, "libraryVersionSelector can not be null");
+    }
+
+    protected IRepository getRepository() {
+        return this.repository;
+    }
+
+    protected FhirContext getFhirContext() {
+        return this.fhirContext;
+    }
+
+    @Override
+    protected IBaseResource getLibrary(ModelIdentifier modelIdentifier) {
+        VersionedIdentifier libraryIdentifier = toLibraryIdentifier(modelIdentifier);
+
+        // TODO: Support lookup by URL...
+
+        @SuppressWarnings("unchecked")
+        var bt = (Class<IBaseBundle>)
+            this.fhirContext.getResourceDefinition("Bundle").getImplementingClass();
+        var lt = this.fhirContext.getResourceDefinition("Library").getImplementingClass();
+
+        var libs = repository.search(
+            bt, lt, Searches.byNameAndVersion(libraryIdentifier.getId(), libraryIdentifier.getVersion()));
+
+        var iter = new BundleIterable<>(repository, libs).iterator();
+
+        if (!iter.hasNext()) {
+            return null;
+        }
+
+        var libraries = new ArrayList<IBaseResource>();
+        iter.forEachRemaining(x -> libraries.add(x.getResource()));
+
+        return this.libraryVersionSelector.select(libraryIdentifier, libraries);
+    }
+
+    protected VersionedIdentifier toLibraryIdentifier(ModelIdentifier modelIdentifier) {
+        VersionedIdentifier result =
+            new VersionedIdentifier()
+                .withSystem(modelIdentifier.getSystem())
+                .withId(modelIdentifier.getId())
+                .withVersion(modelIdentifier.getVersion());
+    }
+}

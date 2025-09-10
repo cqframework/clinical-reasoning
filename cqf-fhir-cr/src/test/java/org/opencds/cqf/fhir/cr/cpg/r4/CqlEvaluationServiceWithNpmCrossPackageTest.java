eb22e0c1ad3f3061776a4d749a710d9633882ad2
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2025-09-10 17:06:30 -0400

    Start setting up library eval cross-package NPM test.

--- cqf-fhir-cr/src/test/java/org/opencds/cqf/fhir/cr/cpg/r4/CqlEvaluationServiceWithNpmCrossPackageTest.java
+++ cqf-fhir-cr/src/test/java/org/opencds/cqf/fhir/cr/cpg/r4/CqlEvaluationServiceWithNpmCrossPackageTest.java
@@ -3,14 +3,117 @@ package org.opencds.cqf.fhir.cr.cpg.r4;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.opencds.cqf.fhir.utility.r4.Parameters.datePart;
 import static org.opencds.cqf.fhir.utility.r4.Parameters.parameters;
+import static org.opencds.cqf.fhir.utility.r4.Parameters.stringPart;
 
 import org.hl7.fhir.r4.model.*;
 import org.hl7.fhir.r4.model.Parameters.ParametersParameterComponent;
 import org.junit.jupiter.api.Test;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class CqlEvaluationServiceWithNpmCrossPackageTest {
 
-    private static final Library.Given GIVEN_REPO = Library.given().repositoryPlusNpmFor("libraryevalnpmcomplex");
+    private static final Library.Given GIVEN_REPO = Library.given().repositoryPlusNpmFor("libraryevalmultilibcrosspackagenpm");
+
+    // LUKETODO:  queries by ID are not supported for NPM
+    @Test
+    void crossPackageSource1() {
+
+        // LUKETODO:  talk to JP about this:  shouyld we support an evaluate library by URL REST API?
+        var params = parameters(stringPart("subject", "Patient/patient1"));
+        var when = GIVEN_REPO
+            .when()
+            .url(new CanonicalType("http://multilib.cross.package.source.npm.opencds.org/Library/MultiLibCrossPackageSource1"))
+            .parameters(params)
+            .evaluateLibrary();
+        var report = when.then().parameters();
+
+        assertNotNull(report);
+        assertTrue(report.hasParameter("Initial Population"));
+        assertTrue(report.hasParameter("Numerator"));
+        assertTrue(report.hasParameter("Denominator"));
+
+        var initialPopulations = report.getParameters("Initial Population");
+        assertNotNull(initialPopulations);
+        assertFalse(initialPopulations.isEmpty());
+        assertEquals(10, initialPopulations.size());
+
+        initialPopulations.forEach(component -> {
+            assertNotNull(component);
+            assertNull(component.getValue());
+            assertEquals("Initial Population", component.getName());
+            assertInstanceOf(Encounter.class, component.getResource());
+        });
+
+        var expectedInitialPopulationEncounterIds = Set.of(
+            "enc_planned_cat1",
+            "enc_cancelled_cat3",
+            "enc_planned_cat2",
+            "enc_triaged_cat3",
+            "enc_planned_cat3",
+            "enc_arrived_cat1_v2",
+            "enc_arrived_cat3",
+            "enc_arrived_cat1_v1",
+            "enc_arrived_cat2",
+            "enc_finished_cat3");
+
+        assertEquals(
+            expectedInitialPopulationEncounterIds,
+            initialPopulations.stream()
+                .map(Parameters.ParametersParameterComponent::getResource)
+                .map(r -> r.getIdElement().getIdPart())
+                .collect(Collectors.toUnmodifiableSet()));
+
+        var denominators = report.getParameters("Denominator");
+        assertNotNull(denominators);
+        assertFalse(denominators.isEmpty());
+        assertEquals(7, denominators.size());
+
+        denominators.forEach(component -> {
+            assertNotNull(component);
+            assertNull(component.getValue());
+            assertEquals("Denominator", component.getName());
+            assertInstanceOf(Encounter.class, component.getResource());
+        });
+
+        var expectedDenominatorEncounterIds = Set.of(
+            "enc_planned_cat1",
+            "enc_planned_cat2",
+            "enc_planned_cat3",
+            "enc_arrived_cat1_v2",
+            "enc_arrived_cat3",
+            "enc_arrived_cat1_v1",
+            "enc_arrived_cat2");
+
+        assertEquals(
+            expectedDenominatorEncounterIds,
+            denominators.stream()
+                .map(Parameters.ParametersParameterComponent::getResource)
+                .map(r -> r.getIdElement().getIdPart())
+                .collect(Collectors.toUnmodifiableSet()));
+
+        var numerators = report.getParameters("Numerator");
+        assertNotNull(numerators);
+        assertFalse(numerators.isEmpty());
+        assertEquals(3, numerators.size());
+
+        numerators.forEach(component -> {
+            assertNotNull(component);
+            assertNull(component.getValue());
+            assertEquals("Numerator", component.getName());
+            assertInstanceOf(Encounter.class, component.getResource());
+        });
+
+        var expectedNumeratorEncounterIds = Set.of("enc_planned_cat1", "enc_planned_cat2", "enc_planned_cat3");
+
+        assertEquals(
+            expectedNumeratorEncounterIds,
+            numerators.stream()
+                .map(Parameters.ParametersParameterComponent::getResource)
+                .map(r -> r.getIdElement().getIdPart())
+                .collect(Collectors.toUnmodifiableSet()));
+    }
+
 
     @Test
     void libraryEvaluationService_inlineAsthma_npm() {

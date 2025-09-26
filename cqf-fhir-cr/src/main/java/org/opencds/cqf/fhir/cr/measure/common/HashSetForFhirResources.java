fc5e66d85b9f948d02152419d67cb42857cb91cd
Luke deGruchy <luke.degruchy@smiledigitalhealth.com>
2025-09-26 17:06:17 -0400

    Fix a failure where resources with the same IDs were getting added multiple times because their object IDs where different but overriding HashSet.add().  Two more test failures remain in the cr module.

--- cqf-fhir-cr/src/main/java/org/opencds/cqf/fhir/cr/measure/common/HashSetForFhirResources.java
+++ cqf-fhir-cr/src/main/java/org/opencds/cqf/fhir/cr/measure/common/HashSetForFhirResources.java
@@ -8,9 +8,13 @@ import org.hl7.fhir.instance.model.api.IBaseResource;
 // LUKETODO:  unit tests
 public class HashSetForFhirResources<T> extends HashSet<T> {
 
+    // This logic is triggered by retainAll() and removeAll(), whose behaviour we're trying
+    // to modify to use FHIR resource identity rules
     @Override
     public boolean contains(Object other) {
-        if (other instanceof IBaseResource otherResource) {
+        final IBaseResource otherResource = castToResourceIfApplicable(other);
+
+        if (otherResource != null) {
             for (T next : this) {
                 if (next instanceof IBaseResource nextResource) {
                     if (areEqual(nextResource, otherResource)) {
@@ -22,6 +26,30 @@ public class HashSetForFhirResources<T> extends HashSet<T> {
         }
 
         return super.contains(other);
+    }
+
+    // If we don't override this logic, we'll get duplicate resources since the comparison to
+    // existing resources in the set will be based on object identity, not FHIR resource identity
+    @Override
+    public boolean add(T newElement) {
+        final IBaseResource newElementResource = castToResourceIfApplicable(newElement);
+
+        if (newElementResource != null) {
+            if (this.contains(newElementResource)) {
+                return false;
+            } else {
+                return super.add(newElement);
+            }
+        }
+
+        return super.add(newElement);
+    }
+
+    private IBaseResource castToResourceIfApplicable(Object obj) {
+        if (obj instanceof IBaseResource resource) {
+            return resource;
+        }
+        return null;
     }
 
     private static boolean areEqual(IBaseResource resource1, IBaseResource resource2) {

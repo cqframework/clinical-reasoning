d677adca57b80bc5f24fb8d131d3817262a89107
Jonathan Percival <jonathan.i.percival@gmail.com>
2025-11-13 08:59:29 -0700

    Path updates

--- cqf-fhir-test/src/main/java/org/opencds/cqf/fhir/test/Resources.java
+++ cqf-fhir-test/src/main/java/org/opencds/cqf/fhir/test/Resources.java
@@ -1,6 +1,5 @@
 package org.opencds.cqf.fhir.test;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -13,6 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 
@@ -28,20 +28,76 @@ public class Resources {
     /**
      * This method returns the real, on-disk path of the resource directory for the given class.
      * @param clazz the class to use to find the resource directory for
-     * @return
+     * @return the on-disk path for the class resources
      */
     public static String getResourcePath(Class<?> clazz) {
-        return new File(clazz.getProtectionDomain()
-                        .getCodeSource()
-                        .getLocation()
-                        .getPath())
-                .getAbsolutePath();
+       return Path.of(getResourceUri(clazz)).toString();
+    }
+
+    /**
+     * This method returns the real, physical URI of the resource directory for the given class.
+     * @param clazz the class to use to find the resource directory for
+     * @return the URI
+     */
+    public static URI getResourceUri(Class<?> clazz) {
+        try {
+            return getResourcePath(clazz, "");
+        }
+        catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
+    /**
+     * This method returns the real, on-disk path of the resource directory for the given class.
+     * @param clazz the class to use to find the resource directory for
+     * @param sourcePackage the source package to use as the root
+     * @return the on-disk path for the class resources of the sourcePackage
+     */
+    public static URI getResourcePath(Class<?> clazz, String sourcePackage) throws URISyntaxException {
+        var clazzPackage = clazz.getPackageName();
+        var targetPackage = sourcePackage == null || sourcePackage.isEmpty() ?
+            "/" + clazzPackage.replace(".", "/") :
+            sourcePackage;
+        var resourceUrl = clazz.getResource(targetPackage);
+        if (resourceUrl == null) {
+            var msg = "Unable to determine resource url for class %s and sourcePackage %s"
+                .formatted(clazz.getSimpleName(), sourcePackage);
+            throw new IllegalArgumentException(msg);
+        }
+        var uri = resourceUrl.toURI();
+        // Handle some gradle-specific shenanigans, which is that under dev gradle splits
+        // the classes and resources into separate directories: build/classes and build/resources
+        var trace = String.join(
+            ",",
+            Arrays.stream(
+                Thread.currentThread().getStackTrace())
+                .map(StackTraceElement::toString).toList());
+        boolean runningInGradle =
+            trace.contains("org.gradle") ||
+                System.getProperty("java.class.path", "").contains("gradle");
+
+        if (runningInGradle && uri.getPath().contains("build/classes/java")) {
+            var newPath = uri.getPath()
+                .replace("build/classes/java", "build/resources");
+            uri = new URI(
+                uri.getScheme(),
+                uri.getUserInfo(),
+                uri.getHost(),
+                uri.getPort(),
+                newPath,
+                uri.getQuery(),
+                uri.getFragment()
+            );
+        }
+
+        return uri;
     }
 
     /**
      * This method copies a resource directory from a jar into a target directory.
      * This is useful for testing, as it allows us to use a export jar resources.
-     *
+     * <p>
      * This method defaults to using the caller class to locate the resource package.
      *
      * @param sourcePackage the source package to copy
@@ -52,13 +108,15 @@ public class Resources {
      */
     public static void copyFromJar(final String sourcePackage, Path target)
             throws URISyntaxException, IOException, ClassNotFoundException {
-        copyFromJar(getCallerClass(2), sourcePackage, target);
+        var clazz = getCallerClass(2);
+
+        copyFromJar(clazz, sourcePackage, target);
     }
 
     /**
      * This method copies a resource directory from a jar into a target directory.
      * This is useful for testing, as it allows us to use a export jar resources.
-     *
+     * <p>
      * This method allows the caller to specify the class to use.
      *
      * @param clazz the class to use for locating the resource package
@@ -69,10 +127,11 @@ public class Resources {
      */
     public static void copyFromJar(final Class<?> clazz, final String sourcePackage, final Path target)
             throws URISyntaxException, IOException {
-        URI resource = clazz.getResource(sourcePackage).toURI();
-        try (var pathReference = PathReference.getPath(resource)) {
-            final Path jarPath = pathReference.getPath();
-            Files.walkFileTree(jarPath, new SimpleFileVisitor<Path>() {
+
+        var uri = getResourcePath(clazz, sourcePackage);
+        try (var pathReference = PathReference.getPath(uri)) {
+            final Path jarPath = pathReference.path();
+            Files.walkFileTree(jarPath, new SimpleFileVisitor<>() {
 
                 @Override
                 public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
@@ -99,38 +158,26 @@ public class Resources {
         return Class.forName(rawFQN.substring(0, rawFQN.lastIndexOf('.')));
     }
 
-    static class PathReference implements AutoCloseable {
-
-        private final Path path;
-        private final FileSystem fs;
-
-        public PathReference(Path path, FileSystem fs) {
-            this.path = path;
-            this.fs = fs;
-        }
+    record PathReference(Path path, FileSystem fs) implements AutoCloseable {
 
         public void close() throws IOException {
-            if (this.fs != null) {
-                this.fs.close();
+                if (this.fs != null) {
+                    this.fs.close();
+                }
+            }
+
+            public static PathReference getPath(final URI resPath) throws IOException {
+                try {
+                    // first try getting a path via existing file systems
+                    return new PathReference(Path.of(resPath), null);
+                } catch (final FileSystemNotFoundException e) {
+                    /*
+                     * not directly on file system, so then it's somewhere else (e.g.: JAR)
+                     */
+                    final Map<String, ?> env = Collections.emptyMap();
+                    final FileSystem fs = FileSystems.newFileSystem(resPath, env);
+                    return new PathReference(fs.provider().getPath(resPath), fs);
+                }
             }
         }
-
-        public Path getPath() {
-            return this.path;
-        }
-
-        public static PathReference getPath(final URI resPath) throws IOException {
-            try {
-                // first try getting a path via existing file systems
-                return new PathReference(Path.of(resPath), null);
-            } catch (final FileSystemNotFoundException e) {
-                /*
-                 * not directly on file system, so then it's somewhere else (e.g.: JAR)
-                 */
-                final Map<String, ?> env = Collections.emptyMap();
-                final FileSystem fs = FileSystems.newFileSystem(resPath, env);
-                return new PathReference(fs.provider().getPath(resPath), fs);
-            }
-        }
-    }
 }

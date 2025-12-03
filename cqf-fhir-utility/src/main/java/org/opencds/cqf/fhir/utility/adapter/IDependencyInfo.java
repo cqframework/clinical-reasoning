bcf6f6da6c8c64c7acd1512d77cf9272c64dfe0f
c-schuler <hoofschu@gmail.com>
2025-12-03 10:08:51 -0700

    Clarify canonical reference contract for IDependencyInfo and enforce normalization in release workflow

--- cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/adapter/IDependencyInfo.java
+++ cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/adapter/IDependencyInfo.java
@@ -3,13 +3,33 @@ package org.opencds.cqf.fhir.utility.adapter;
 import java.util.List;
 import org.hl7.fhir.instance.model.api.IBaseExtension;
 
+
+/**
+ * Represents a dependency on another FHIR artifact.
+ * <p>
+ * The {@code reference} property is intended to be a FHIR canonical reference (i.e., {@code url} or {@code url|version})
+ * as defined by the FHIR specification. Implementations are responsible for resolving any non-canonical references
+ * to canonical form before returning them via this interface.
+ */
 public interface IDependencyInfo {
     String getReferenceSource();
 
     void setReferenceSource(String referenceSource);
 
+    /**
+     * Returns the canonical reference string (url or url|version) for the dependency.
+     * This must not be a relative reference or a plain id.
+     *
+     * @return the canonical reference string for the dependency
+     */
     String getReference();
 
+    /**
+     * Sets the canonical reference string (url or url|version) for the dependency.
+     * Callers must supply a canonical reference string; implementations may normalize or validate the reference.
+     *
+     * @param reference the canonical reference string for the dependency
+     */
     void setReference(String reference);
 
     String getReferencePackageId();

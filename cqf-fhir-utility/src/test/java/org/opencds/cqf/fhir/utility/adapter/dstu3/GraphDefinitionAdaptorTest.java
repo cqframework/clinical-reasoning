67fb2d8bf156aa385a9c860e75da5e6004b81ceb
leifstawnyczy <leif.stawnyczy@smiledigitalhealth.com>
2025-12-15 09:08:29 -0500

    coverage testing

--- cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/adapter/dstu3/GraphDefinitionAdaptorTest.java
+++ cqf-fhir-utility/src/test/java/org/opencds/cqf/fhir/utility/adapter/dstu3/GraphDefinitionAdaptorTest.java
@@ -4,9 +4,15 @@ import ca.uhn.fhir.context.FhirContext;
 import java.util.Arrays;
 import java.util.List;
 import org.hl7.fhir.dstu3.model.GraphDefinition;
+import org.hl7.fhir.dstu3.model.Reference;
+import org.hl7.fhir.dstu3.model.RelatedArtifact;
 import org.hl7.fhir.dstu3.model.RelatedArtifact.RelatedArtifactType;
+import org.junit.jupiter.api.Test;
 import org.opencds.cqf.fhir.utility.adapter.IAdapterFactory;
+import org.opencds.cqf.fhir.utility.adapter.IGraphDefinitionAdapter;
 import org.opencds.cqf.fhir.utility.adapter.IGraphDefinitionAdaptorTest;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class GraphDefinitionAdaptorTest implements IGraphDefinitionAdaptorTest<GraphDefinition> {
 
@@ -43,5 +49,34 @@ public class GraphDefinitionAdaptorTest implements IGraphDefinitionAdaptorTest<G
         // dstu3 does not set canonical references as urls directly,
         // but wraps them in a reference
         return String.format("{\"reference\":\"%s\"}", ref);
+    }
+
+    @Test
+    public void getReferenceFromArtifact_noResourceRef_coverageTest() {
+        // setup
+        RelatedArtifact artifact = new RelatedArtifact();
+        GraphDefinition definition = new GraphDefinition();
+
+        IGraphDefinitionAdapter adapter = getAdaptorFactory().createGraphDefinition(definition);
+
+        // test
+        String url = adapter.getReferenceFromArtifact(artifact);
+
+        // verify
+        assertNull(url);
+    }
+
+    @Test
+    public void getReferenceFromArtifact_noArtifact_coverageTest() {
+        // setup
+        GraphDefinition definition = new GraphDefinition();
+
+        IGraphDefinitionAdapter adapter = getAdaptorFactory().createGraphDefinition(definition);
+
+        // test
+        String url = adapter.getReferenceFromArtifact(new Reference("http://example.com"));
+
+        // verify
+        assertNull(url);
     }
 }

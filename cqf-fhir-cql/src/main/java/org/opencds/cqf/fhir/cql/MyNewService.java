2a1efb1fc52fb17530f154eeae2ff0b4f1c42839
leifstawnyczy <leif.stawnyczy@smiledigitalhealth.com>
2026-01-23 16:15:29 -0500

    adding some jacoco checks

---
+++ cqf-fhir-cql/src/main/java/org/opencds/cqf/fhir/cql/MyNewService.java
@@ -0,0 +1,38 @@
+package org.opencds.cqf.fhir.cql;
+
+import ca.uhn.fhir.context.FhirContext;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static ca.uhn.fhir.util.ElementUtil.isEmpty;
+
+public class MyNewService {
+
+    FhirContext ctx;
+
+    public MyNewService(FhirContext ctx) {
+        this.ctx = ctx;
+    }
+
+    public IBaseResource runSystem(String resourceName) {
+        if (isEmpty(resourceName)) {
+            throw new RuntimeException("This is an exception");
+        }
+
+        var resourceDefinition = ctx.getResourceDefinition(resourceName);
+
+        return resourceDefinition.newInstance();
+    }
+
+    public void sortMyLists(List<IBaseResource> resource1, List<IBaseResource> resource2) {
+        List<IBaseResource> newRe = new ArrayList<>();
+        for (IBaseResource resource : resource1) {
+            if (!resource2.contains(resource)) {
+                newRe.add(resource);
+            }
+        }
+        resource1.addAll(newRe);
+    }
+}

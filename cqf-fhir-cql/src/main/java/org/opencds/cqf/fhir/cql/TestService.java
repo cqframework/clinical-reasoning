4e93d1c49e3bdeb141704a26ebe19ed8b076c1a1
leifstawnyczy <leif.stawnyczy@smiledigitalhealth.com>
2026-02-02 10:14:52 -0500

    test service

---
+++ cqf-fhir-cql/src/main/java/org/opencds/cqf/fhir/cql/TestService.java
@@ -0,0 +1,31 @@
+package org.opencds.cqf.fhir.cql;
+
+import ca.uhn.fhir.context.FhirContext;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TestService {
+	FhirContext fhirContext;
+	public TestService(FhirContext ctx) {
+		fhirContext = ctx;
+	}
+
+	public List<IBaseResource> mix(List<IBaseResource> first, List<IBaseResource> second) {
+		List<IBaseResource> mixed = new ArrayList<>();
+		Set<String> idsOfFirst = first.stream()
+			.map(r -> r.getIdElement().getValue())
+			.collect(Collectors.toSet());
+		for (IBaseResource resource : second) {
+			if (!idsOfFirst.contains(resource.getIdElement().getValue()) {
+				mixed.add(resource);
+			}
+		}
+		mixed.addAll(first);
+
+		return mixed;
+	}
+}

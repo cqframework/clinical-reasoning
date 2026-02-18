ed8ef34e68e544c5fa2fedbfe9f98b73655bfce0
Brenin Rhodes <brenin.rhodes@smiledigitalhealth.com>
2026-02-18 11:16:10 -0700

    spotless

--- cqf-fhir-cr/src/test/java/org/opencds/cqf/fhir/cr/cql/evaluate/CqlEvaluationRequestTests.java
+++ cqf-fhir-cr/src/test/java/org/opencds/cqf/fhir/cr/cql/evaluate/CqlEvaluationRequestTests.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.opencds.cqf.cql.engine.model.ModelResolver;
 import org.opencds.cqf.fhir.cql.LibraryEngine;
 
 @SuppressWarnings("UnstableApiUsage")

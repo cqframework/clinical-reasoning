fcbf315a3474fe2b22fc47e5d13ebb26e664cc08
c-schuler <hoofschu@gmail.com>
2026-02-26 10:44:47 -0700

    Added CRMI params and tests

--- cqf-fhir-cr-hapi/src/main/java/org/opencds/cqf/fhir/cr/hapi/r4/implementationguide/ImplementationGuideDataRequirements.java
+++ cqf-fhir-cr-hapi/src/main/java/org/opencds/cqf/fhir/cr/hapi/r4/implementationguide/ImplementationGuideDataRequirements.java
@@ -2,6 +2,9 @@ package org.opencds.cqf.fhir.cr.hapi.r4.implementationguide;
 
 import static org.opencds.cqf.fhir.cr.hapi.common.CanonicalHelper.getCanonicalType;
 import static org.opencds.cqf.fhir.cr.hapi.common.IdHelper.getIdType;
+import static org.opencds.cqf.fhir.utility.EndpointHelper.getEndpoint;
+import static org.opencds.cqf.fhir.utility.Resources.newBaseForVersion;
+import static org.opencds.cqf.fhir.utility.adapter.IAdapterFactory.forFhirVersion;
 
 import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.rest.annotation.IdParam;
@@ -10,12 +13,16 @@ import ca.uhn.fhir.rest.annotation.OperationParam;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
 import ca.uhn.fhir.rest.server.provider.ProviderConstants;
+import java.util.List;
 import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.instance.model.api.IBase;
+import org.hl7.fhir.instance.model.api.IBaseParameters;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IPrimitiveType;
+import org.hl7.fhir.r4.model.Endpoint;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.ImplementationGuide;
-import org.hl7.fhir.r4.model.Parameters;
+import org.hl7.fhir.r4.model.Parameters.ParametersParameterComponent;
 import org.hl7.fhir.r4.model.StringType;
 import org.opencds.cqf.fhir.cr.hapi.common.IImplementationGuideProcessorFactory;
 import org.opencds.cqf.fhir.utility.monad.Eithers;
@@ -32,9 +39,16 @@ public class ImplementationGuideDataRequirements {
 
     /**
      * Implements the CRMI <a href="https://build.fhir.org/ig/HL7/crmi-ig/OperationDefinition-crmi-data-requirements.html">$data-requirements</a>
-     * operation for ImplementationGuide resources.
+     * operation for ImplementationGuide resources (instance-level).
      *
      * @param id the id of the ImplementationGuide resource
+     * @param artifactEndpointConfiguration configuration information to resolve canonical artifacts.
+     *                                      Each element contains parts: artifactRoute, endpointUri, endpoint.
+     * @param terminologyEndpoint the FHIR {@link Endpoint} Endpoint resource or url to use to access
+     *                            terminology (i.e. valuesets, codesystems, and membership testing)
+     *                            referenced by the resource. If no terminology endpoint is supplied,
+     *                            the evaluation will attempt to use the server on which the operation
+     *                            is being performed as the terminology server.
      * @param requestDetails the details (such as tenant) of this request. Usually autopopulated by HAPI.
      * @return a module-definition Library describing the data requirements
      */
@@ -42,22 +56,34 @@ public class ImplementationGuideDataRequirements {
             name = ProviderConstants.CR_OPERATION_DATAREQUIREMENTS,
             idempotent = true,
             type = ImplementationGuide.class)
-    public IBaseResource getDataRequirements(@IdParam IdType id, RequestDetails requestDetails)
+    public IBaseResource getDataRequirements(
+            @IdParam IdType id,
+            @OperationParam(name = "artifactEndpointConfiguration")
+                    List<ParametersParameterComponent> artifactEndpointConfiguration,
+            @OperationParam(name = "terminologyEndpoint") ParametersParameterComponent terminologyEndpoint,
+            RequestDetails requestDetails)
             throws InternalErrorException, FHIRException {
         return implementationGuideProcessorFactory
                 .create(requestDetails)
-                .dataRequirements(Eithers.forMiddle3(id), null);
+                .dataRequirements(
+                        Eithers.forMiddle3(id), buildParameters(artifactEndpointConfiguration, terminologyEndpoint));
     }
 
     /**
      * Implements the CRMI <a href="https://build.fhir.org/ig/HL7/crmi-ig/OperationDefinition-crmi-data-requirements.html">$data-requirements</a>
-     * operation for ImplementationGuide resources.
+     * operation for ImplementationGuide resources (type-level).
      *
      * @param id the logical id of the ImplementationGuide resource to analyze
      * @param canonical a canonical reference to the ImplementationGuide resource
      * @param url canonical URL of the ImplementationGuide when invoked at the resource type level
      * @param version version of the ImplementationGuide when invoked at the resource type level
-     * @param parameters any input parameters for the data requirements analysis
+     * @param artifactEndpointConfiguration configuration information to resolve canonical artifacts.
+     *                                      Each element contains parts: artifactRoute, endpointUri, endpoint.
+     * @param terminologyEndpoint the FHIR {@link Endpoint} Endpoint resource or url to use to access
+     *                            terminology (i.e. valuesets, codesystems, and membership testing)
+     *                            referenced by the resource. If no terminology endpoint is supplied,
+     *                            the evaluation will attempt to use the server on which the operation
+     *                            is being performed as the terminology server.
      * @param requestDetails the details (such as tenant) of this request. Usually autopopulated by HAPI.
      * @return a module-definition Library describing the data requirements
      */
@@ -70,7 +96,9 @@ public class ImplementationGuideDataRequirements {
             @OperationParam(name = "canonical", typeName = "canonical") IPrimitiveType<String> canonical,
             @OperationParam(name = "url", typeName = "uri") IPrimitiveType<String> url,
             @OperationParam(name = "version") StringType version,
-            @OperationParam(name = "parameters", max = 1) Parameters parameters,
+            @OperationParam(name = "artifactEndpointConfiguration")
+                    List<ParametersParameterComponent> artifactEndpointConfiguration,
+            @OperationParam(name = "terminologyEndpoint") ParametersParameterComponent terminologyEndpoint,
             RequestDetails requestDetails)
             throws InternalErrorException, FHIRException {
         return implementationGuideProcessorFactory
@@ -80,6 +108,28 @@ public class ImplementationGuideDataRequirements {
                                 getCanonicalType(fhirVersion, canonical, url, version),
                                 getIdType(fhirVersion, "ImplementationGuide", id),
                                 null),
-                        parameters);
+                        buildParameters(artifactEndpointConfiguration, terminologyEndpoint));
+    }
+
+    private IBaseParameters buildParameters(
+            List<ParametersParameterComponent> artifactEndpointConfiguration,
+            ParametersParameterComponent terminologyEndpoint) {
+        if (artifactEndpointConfiguration == null && terminologyEndpoint == null) {
+            return null;
+        }
+        var params = forFhirVersion(fhirVersion)
+                .createParameters((IBaseParameters) newBaseForVersion("Parameters", fhirVersion));
+        if (artifactEndpointConfiguration != null) {
+            for (IBase config : artifactEndpointConfiguration) {
+                params.addParameter(config);
+            }
+        }
+        if (terminologyEndpoint != null) {
+            var endpoint = getEndpoint(fhirVersion, terminologyEndpoint);
+            if (endpoint != null) {
+                params.addParameter("terminologyEndpoint", endpoint);
+            }
+        }
+        return (IBaseParameters) params.get();
     }
 }

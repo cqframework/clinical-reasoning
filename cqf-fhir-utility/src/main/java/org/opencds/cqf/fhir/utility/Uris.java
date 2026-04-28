4ba174a6b95b645bf1657a332a8c6b290dc6b764
Chris0296 <38321098+Chris0296@users.noreply.github.com>
2026-04-28 10:07:49 -0700

    Ensure Authoritative Source Extension is added to Leaf ValueSet during $release (#1009)
    * Ensure Authoritative Source Extension is added to Leaf ValueSets retrieved during $release
    
    * Move ensureHttps from R4ImportBundleProducer to Uris Utility Class
    
    * Sonar Fixes
    
    * Fix Typo
    
    * add version agnostic method
    
    * Fix url value type and cleanup
    
    ---------
    
    Co-authored-by: Brenin Rhodes <brenin.rhodes@smiledigitalhealth.com>

--- cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/Uris.java
+++ cqf-fhir-utility/src/main/java/org/opencds/cqf/fhir/utility/Uris.java
@@ -1,5 +1,10 @@
 package org.opencds.cqf.fhir.utility;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Uris {
     private Uris() {}
 
@@ -17,5 +22,26 @@ public class Uris {
         }
 
         return uri.startsWith("file") || !uri.matches("\\w+?://.*");
+    }
+
+    public static String ensureHttps(String urlString) throws MalformedURLException, URISyntaxException {
+        URL url = URI.create(urlString).toURL();
+
+        // Check if the protocol is already HTTPS
+        if ("https".equalsIgnoreCase(url.getProtocol())) {
+            return urlString;
+        }
+
+        // Construct a new URL with the HTTPS protocol
+        URI httpsUrl = new URI(
+                "https", // scheme
+                null, // userinfo
+                url.getHost(), // host
+                url.getPort(), // port
+                url.getFile(), // path
+                null, // query
+                null // fragment
+                );
+        return httpsUrl.toString();
     }
 }

b767ef503cf360865e3f576fa6edf74a521a061f
c-schuler <hoofschu@gmail.com>
2026-09-08 14:20:17 -0600

    Spotless and added tests

--- cqf-fhir-cr/src/main/java/org/opencds/cqf/fhir/cr/measure/common/FunctionEvaluationHandler.java
+++ cqf-fhir-cr/src/main/java/org/opencds/cqf/fhir/cr/measure/common/FunctionEvaluationHandler.java
@@ -4,7 +4,6 @@ import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -410,8 +409,8 @@ public class FunctionEvaluationHandler {
             var expressionName = popDef.id() + "-" + stratifierExpression;
             final List<FunctionResultEntry> functionResults = new ArrayList<>();
             // Identity-keyed: these accumulate engine values whose hashCode walks the whole
-        // element tree. See HashSetForFhirResourcesAndCqlTypes.
-        final Set<Value> evaluatedResources = new HashSetForFhirResourcesAndCqlTypes<>();
+            // element tree. See HashSetForFhirResourcesAndCqlTypes.
+            final Set<Value> evaluatedResources = new HashSetForFhirResourcesAndCqlTypes<>();
 
             for (var result : resultsIter) {
                 final ExpressionResult functionResult = evaluateNonSubValueStratifiersFunction(

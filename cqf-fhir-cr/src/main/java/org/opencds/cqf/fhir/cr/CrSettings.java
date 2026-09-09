3a069054f46a4d3699986ba75f2fcabe745fa2c3
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2026-09-09 20:05:04 +0200

    Adding abstraction to bring your own validation (#1094)
    * Adding abstraction to bring your own validation
    
    * resolved spotless

--- cqf-fhir-cr/src/main/java/org/opencds/cqf/fhir/cr/CrSettings.java
+++ cqf-fhir-cr/src/main/java/org/opencds/cqf/fhir/cr/CrSettings.java
@@ -1,11 +1,13 @@
 package org.opencds.cqf.fhir.cr;
 
 import org.opencds.cqf.fhir.cql.EvaluationSettings;
+import org.opencds.cqf.fhir.utility.IResourceValidator;
 import org.opencds.cqf.fhir.utility.client.TerminologyServerClientSettings;
 
 public class CrSettings {
     private EvaluationSettings evaluationSettings;
     private TerminologyServerClientSettings terminologyServerClientSettings;
+    private IResourceValidator resourceValidator;
 
     public static CrSettings getDefault() {
         return new CrSettings();
@@ -14,6 +16,7 @@ public class CrSettings {
     public CrSettings() {
         evaluationSettings = EvaluationSettings.getDefault();
         terminologyServerClientSettings = TerminologyServerClientSettings.getDefault();
+        resourceValidator = null;
     }
 
     public EvaluationSettings getEvaluationSettings() {
@@ -41,5 +44,31 @@ public class CrSettings {
 
     public void setTerminologyServerClientSettings(TerminologyServerClientSettings terminologyServerClientSettings) {
         this.terminologyServerClientSettings = terminologyServerClientSettings;
+    }
+
+    /**
+     * Returns the configured resource validator, or null if none has been set.
+     * When null, components should bootstrap their own validator as needed.
+     *
+     * @return the configured IResourceValidator, or null
+     */
+    public IResourceValidator getResourceValidator() {
+        return resourceValidator;
+    }
+
+    /**
+     * Sets a custom resource validator to be used instead of bootstrapping one internally.
+     * This allows external configuration of the FHIR validation support chain.
+     *
+     * @param resourceValidator the validator to use
+     * @return this CrSettings instance for fluent chaining
+     */
+    public CrSettings withResourceValidator(IResourceValidator resourceValidator) {
+        this.resourceValidator = resourceValidator;
+        return this;
+    }
+
+    public void setResourceValidator(IResourceValidator resourceValidator) {
+        this.resourceValidator = resourceValidator;
     }
 }

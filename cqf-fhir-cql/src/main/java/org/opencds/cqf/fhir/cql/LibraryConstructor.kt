6ad397a112efd88f5d8b895bc9d24b5229753764
Brenin Rhodes <brenin.rhodes@smiledigitalhealth.com>
2026-09-19 13:00:55 -0400

    spotless

--- cqf-fhir-cql/src/main/java/org/opencds/cqf/fhir/cql/LibraryConstructor.kt
+++ cqf-fhir-cql/src/main/java/org/opencds/cqf/fhir/cql/LibraryConstructor.kt
@@ -8,7 +8,10 @@ import org.opencds.cqf.fhir.utility.FhirPathCache
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class LibraryConstructor(protected var fhirContext: FhirContext, protected var evaluationSettings: EvaluationSettings) {
+class LibraryConstructor(
+    protected var fhirContext: FhirContext,
+    protected var evaluationSettings: EvaluationSettings,
+) {
     protected var fhirPath: IFhirPath = FhirPathCache.cachedForContext(fhirContext)
 
     fun constructCqlLibrary(
@@ -58,9 +61,9 @@ class LibraryConstructor(protected var fhirContext: FhirContext, protected var e
     }
 
     private fun constructIncludes(sb: StringBuilder, libraries: MutableMap<String?, String?>?) {
-        var fhirHelpersInclude = "FHIRHelpers";
+        var fhirHelpersInclude = "FHIRHelpers"
         if (evaluationSettings.registeredNamespaces.contains("hl7.fhir.uv.cql")) {
-            fhirHelpersInclude = "hl7.fhir.uv.cql.".plus(fhirHelpersInclude);
+            fhirHelpersInclude = "hl7.fhir.uv.cql.".plus(fhirHelpersInclude)
         }
         sb.append(
             "include $fhirHelpersInclude version '${getFhirVersionString(fhirContext.version.version)}' called FHIRHelpers\n"
